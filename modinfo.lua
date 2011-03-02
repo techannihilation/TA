d7168de47dffa134b4a0e9f78b2f479f6b742efa
newmind <git.newmind@newmind.at.eu.org>
2011-03-02 01:15:58 +0100

    changed mod version to 2.02-dev

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
   name='Tech Annihilation',
   description='The New Generation Of War',
   shortname='TA',
-  version='v2.01',
+  version='v2.02-dev',
   mutator='Official',
   game='Total Annihilation',
   shortGame='TA',
