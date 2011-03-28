991647bda13b54f03be00518cd119c9224190103
nixtux <davehomer@googlemail.com>
2011-03-28 19:30:48 +0100

    Start making mod dependent on ba again

--- modinfo.lua
+++ modinfo.lua
@@ -2,9 +2,12 @@ return {
   name='Tech Annihilation',
   description='The New Generation Of War',
   shortname='TA',
-  version='v2.04-dev',
+  version='v2.04-dev-dep',
   mutator='Official',
   game='Total Annihilation',
   shortGame='TA',
   modtype=1,
+ depend = {
+    'Balanced Annihilation V7.20',
+  }
 }
