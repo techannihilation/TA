1078172fd028cce285daccc5a3c317d8b0093013
nixtux <davehomer@googlemail.com>
2011-04-18 16:45:09 +0100

    move to using sidedata.lua not sidedata.tdf

---
+++ gamedata/sidedata.lua
@@ -0,0 +1,15 @@
+
+return {
+    {
+        name = 'ARM',
+        startunit = 'armcom',
+    },
+    {
+        name = 'CORE',
+        startunit = 'corcom',
+    },
+      {
+        name = 'TLL',
+        startunit = 'tllcom',
+    },
+}
