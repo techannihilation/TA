e3ae7ef99d936578a70ccac69fdcaa133ca504da
nixtux <davehomer@googlemail.com>
2011-04-20 23:29:04 +0100

    Checked to see if i missed anything

--- modinfo.lua
+++ modinfo.lua
@@ -2,12 +2,12 @@ return {
   name='Tech Annihilation',
   description='The New Generation Of War',
   shortname='TA',
-  version='v2.06',
+  version='v2.07',
   mutator='',
   game='',
   shortGame='',
   modtype=1,
   depend = {
-    'Balanced Annihilation V7.20',
-	}, 
+    'Balanced Annihilation V7.31',
+  }, 
   }
