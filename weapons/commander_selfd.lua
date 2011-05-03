b28f5677107bef47fb24cd04b07a3ff15c058fe7
nixtux <davehomer@googlemail.com>
2011-05-03 20:26:38 +0100

    Add missing comm explosion [commander_selfd]

---
+++ weapons/commander_selfd.lua
@@ -0,0 +1,25 @@
+return {
+        commander_selfd = {
+        areaofeffect = 1410,
+        ballistic = 1,
+        craterboost = 6,
+        cratermult = 3,
+        edgeeffectiveness = 0.25,
+        explosiongenerator = "custom:FLASHNUKE1280",
+        impulseboost = 3,
+        impulsefactor = 3,
+        name = "Matter/AntimatterExplosion",
+        range = 880,
+        reloadtime = 3.6,
+        rendertype = 4,
+        shakeduration = 5,
+        shakemagnitude = 600,
+        soundhit = "newboom",
+        soundstart = "largegun",
+        turret = 1,
+        weaponvelocity = 250,
+        damage = {
+        default = 25000,
+        },
+  },
+  }
