c3298ec9e9fbcce576dcefb04b8f84d88935761c
nixtux <davehomer@googlemail.com>
2011-05-08 19:51:09 +0100

    Add jumpdef setting for tllcomms

--- luarules/configs/jump_defs.lua
+++ luarules/configs/jump_defs.lua
@@ -43,4 +43,25 @@ return {
     reload    = 15, -- optional
     aaShootMe = false, -- optional
   },
+    tllcom5 = {
+    range     = 950,
+    height    = 100,
+    speed     = 3, 
+    reload    = 40, -- optional
+    aaShootMe = false, -- optional
+  },
+    tllcom6 = {
+    range     = 1100,
+    height    = 150,
+    speed     = 4, 
+    reload    = 20, -- optional
+    aaShootMe = false, -- optional
+  },
+    tllcom7 = {
+    range     = 1250,
+    height    = 150,
+    speed     = 5, 
+    reload    = 15, -- optional
+    aaShootMe = false, -- optional
+  },
 }
