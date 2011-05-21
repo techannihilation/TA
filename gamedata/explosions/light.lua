8fe94d6e2353237bca2a9b4319d1d0e077873b54
nixtux <davehomer@googlemail.com>
2011-05-21 18:49:58 +0100

    Merge tllweb branch

--- gamedata/explosions/light.lua
+++ gamedata/explosions/light.lua
@@ -1,7 +1,9 @@
 -- gantwhitelight
+-- webbluelight
 -- smallwhitelight
 -- whitelight
 -- yellowlight
+-- bluelight
 
 return {
   ["gantwhitelight"] = {
@@ -22,12 +24,37 @@ return {
         sizemod            = 0,
         sizemodmod         = 0,
         speed              = [[0.05 r-0.1, 0.05 r-0.1, 0.05 r-0.1]],
-        texture            = [[WhiteLight]],
+        texture            = [[whitelight]],
+        useairlos          = false,
+      },
+    },
+  },
+  
+ ["webbluelight"] = {
+    useairlos          = false,
+    usedefaultexplosions = false,
+    heatcloud = {
+      air                = true,
+      count              = 3,
+      ground             = true,
+      water              = true,
+      properties = {
+        heat               = 35,
+        heatfalloff        = 0.54,
+        maxheat            = 35,
+        pos                = [[0, 0.0, 0]],
+        size               = [[15.0 r1]],
+        sizegrowth         = [[0.09 r.16]],
+        sizemod            = 0,
+        sizemodmod         = 0,
+        speed              = [[0.04 r-0.1, 0.04 r-0.1, 0.04 r-0.1]],
+        texture            = [[blueexploredexplo]],
         useairlos          = false,
       },
     },
   },
 
+  
   ["smallwhitelight"] = {
     useairlos          = false,
     usedefaultexplosions = false,
@@ -46,7 +73,7 @@ return {
         sizemod            = 0,
         sizemodmod         = 0,
         speed              = [[0.05 r-0.1, 0.05 r-0.1, 0.05 r-0.1]],
-        texture            = [[RedLight]],
+        texture            = [[yellowlight]],
         useairlos          = false,
       },
     },
@@ -76,6 +103,7 @@ return {
     },
   },
 
+  
   ["yellowlight"] = {
     useairlos          = false,
     usedefaultexplosions = false,
@@ -101,5 +129,28 @@ return {
     },
   },
 
+   ["bluelight"] = {
+    useairlos          = false,
+    usedefaultexplosions = false,
+    heatcloud = {
+      air                = true,
+      count              = 2,
+      ground             = true,
+      water              = true,
+      properties = {
+        heat               = 25,
+        heatfalloff        = 1.14,
+        maxheat            = 25,
+        pos                = [[0, 0.0, 0]],
+        size               = [[12.0 r1]],
+        sizegrowth         = [[0.08 r.16]],
+        sizemod            = 0,
+        sizemodmod         = 0,
+        speed              = [[0.05 r-0.1, 0.05 r-0.1, 0.05 r-0.1]],
+        texture            = [[purpleexplowebblue]],
+        useairlos          = false,
+      },
+    },
+  },
 }
 
