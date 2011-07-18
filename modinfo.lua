81aacfb7ac008b89f8df7d3d3f4c7002ba40d097
newmind <git.newmind@newmind.at.eu.org>
2011-07-18 23:20:47 +0200

    2.11-rc3

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
   name='Tech Annihilation',
   description='The New Generation Of War',
   shortname='TA',
-  version='v2.11-rc2',
+  version='v2.11-rc3',
   mutator='',
   game='',
   shortGame='',
