9397d10fc03716fd345d73bf025d69cbd295d862
nixtux <davehomer@googlemail.com>
2011-11-18 14:41:41 -0800

    Update CollisionVolumes gadget with latest version

--- luarules/configs/collisionvolumes.lua
+++ luarules/configs/collisionvolumes.lua
@@ -1,4 +1,4 @@
---[[  from Spring Wiki, info about CollisionVolumeData
+--[[  from Spring Wiki and source code, info about CollisionVolumeData
 Spring.GetUnitCollisionVolumeData ( number unitID ) -> 
 	number scaleX, number scaleY, number scaleZ, number offsetX, number offsetY, number offsetZ,
 	number volumeType, number testType, number primaryAxis, boolean disabled
@@ -7,6 +7,11 @@ Spring.SetUnitCollisionVolumeData ( number unitID, number scaleX, number scaleY,
 					number offsetX, number offsetY, number offsetX,
 					number vType, number tType, number Axis ) -> nil
 
+Spring.SetUnitPieceCollisionVolumeData ( number unitID, number pieceIndex, boolean enabled, number scaleX, number scaleY, number scaleZ,
+					number offsetX, number offsetY, number offsetZ, number vType, number Axis) -> nil
+	per piece collision volumes always use COLVOL_TEST_CONT as tType
+	above syntax is for 0.83, for 0.82 compatibility repeat enabled 3 more times
+	
    possible vType constants
      DISABLED = -1  disables collision volume and collision detection for that unit, do not use
      ELLIPSOID = 0
@@ -23,11 +28,31 @@ Spring.SetUnitCollisionVolumeData ( number unitID, number scaleX, number scaleY,
      COLVOL_AXIS_X = 0
      COLVOL_AXIS_Y = 1
      COLVOL_AXIS_Z = 2
+
+   sample collision volume with detailed descriptions
+	unitCollisionVolume["arm_advanced_radar_tower"] = {
+		on=            -- Unit is active/open/poped-up 
+		   {60,80,60,  -- Volume X scale, Volume Y scale, Volume Z scale,
+		    0,15,0,    -- Volume X offset, Volume Y offset, Volume Z offset,
+		    0,1,0},    -- vType, tType, axis}
+		off={32,48,32,0,-10,0,0,1,0},
+	}
+	pieceCollisionVolume["arm_big_bertha"] = {
+		["1"]={true,       -- [pieceIndexNumber]={enabled,
+			   48,74,48,   --              Volume X scale, Volume Y scale, Volume Z scale,
+		       0,0,0,      --              Volume X offset, Volume Y offset, Volume Z offset,
+			   1,1}        --              vType, axis},
+		["2"]={false},
+		....
+	}
+
 ]]--
 
---Collision volume definitions, ones entered here are for BA, for other mods modify apropriatly
 
-local unitCollisionVolume = {}
+--Collision volume definitions, ones entered here are for TA, for other mods modify apropriatly
+local unitCollisionVolume = {}			--dynamic collision volume definitions
+local pieceCollisionVolume = {}			--per piece collision volume definitions
+local dynamicPieceCollisionVolume = {}	--dynamic per piece collision volume definitions
 
 	unitCollisionVolume["armarad"] = {
 		on={66,80,66,0,15,0,0,1,0},    --{Xscale,Yscale,Zscale, Xoffset,Yoffset,Zoffset, vType,tType,axis}
@@ -45,57 +70,139 @@ local unitCollisionVolume = {}
 		on={54,81,54,0,-2,0,2,1,0},
 		off={54,56,54,0,-15,0,2,1,0},
 	}
+	unitCollisionVolume["armlab"] = {
+		on={95,28,95,0,2,0,2,1,0},
+		off={95,22,95,0,-1,0,1,1,1},
+	}
 	unitCollisionVolume["armmmkr"] = {
 		on={60,72,60,0,7,0,1,1,1},
 		off={60,50,60,0,-4,0,1,1,1},
 	}
 	unitCollisionVolume["armpb"] = {
-		on={39,88,39,0,-14,0,0,1,0},
-		off={39,53,39,0,-20,0,0,1,0},
+		on={39,88,39,0,-11,0,0,1,0},
+		off={39,55,39,0,-27,0,0,1,0},
 	}
 	unitCollisionVolume["armplat"] = {
 		on={105,66,105,0,33,0,2,1,0},
 		off={105,44,105,0,0,0,2,1,0},
 	}
 	unitCollisionVolume["armsolar"] = {
-		on={83,100,83,0,-12,1,0,1,0},
-		off={50,95,50,0,-12,1,0,1,0},
+		on={83,76,83,0,-18,1,0,1,0},
+		off={50,76,50,0,-18,1,0,1,0},
 	}
 	unitCollisionVolume["armtarg"] = {
 		on={62,34,62,0,0,0,2,1,0},
-		off={55,78,55,0,-5.5,0,0,1,0},
+		off={55,78,55,0,-19.5,0,0,1,0},
+	}
+	unitCollisionVolume["armvp"] = {
+		on={120,34,92,0,0,0,2,1,0},
+		off={90,34,92,0,0,0,2,1,0},
 	}
 	unitCollisionVolume["cordoom"] = {
-		on={55,105,55,0,-3,0,2,1,0},
-		off={48,86,48,0,-15,0,2,1,0},
+		on={63,112,63,0,12,0,1,1,1},
+		off={45,87,45,0,0,0,2,1,0},
+	}
+	unitCollisionVolume["corfmkr"] = {
+		on={48,46,48,0,0,0,0,1,0},
+		off={48,43,48,0,-16,0,0,1,0},
+	}
+	unitCollisionVolume["corgant"] = {
+		on={118,96,130,0,-21,0,1,1,2},
+		off={110,66,130,0,-21,0,1,1,2},
+	}
+	unitCollisionVolume["cormexp"] = {
+		on={83,77,78,0,-27,0,0,1,0},
+		off={90,135,84,0,-27,0,0,1,0},
 	}
 	unitCollisionVolume["cormmkr"] = {
 		on={60,60,60,0,0,0,1,1,1},
-		off={50,92,50,0,-17.5,0,0,1,0},
+		off={50,92,50,0,-22.5,0,0,1,0},
 	}
 	unitCollisionVolume["corplat"] = {
-		on={112,60,112,0,30,0,1,1,1},
-		off={112,37,112,0,0,0,1,1,1},
+		on={112,60,112,0,28,0,1,1,1},
+		off={112,35,112,0,0,0,1,1,1},
 	}
 	unitCollisionVolume["corsolar"] = {
-		on={86,43,86,0,-10,0,0,1,0},
-		off={57,32,57,0,-10,0,0,1,0},
+		on={86,78,86,0,-25,0,0,1,0},
+		off={77,78,77,0,-35,0,0,1,0},
 	}
 	unitCollisionVolume["cortarg"] = {
 		on={64,20,64,0,0,0,1,1,1},
 		off={38,20,38,0,0,0,1,1,1},
 	}
 	unitCollisionVolume["cortoast"] = {
-		on={44,23,44,0,0,0,2,1,0},
-		off={44,10,44,0,-7,0,2,1,0},
+		on={44,23,44,0,4,0,2,1,0},
+		off={44,7,44,0,-3.3,0,2,1,0},
 	}
 	unitCollisionVolume["corvipe"] = {
-		on={39,86,39,0,-13,0,0,1,0},
-		off={39,35,39,0,-17,0,0,1,0},
+		on={39,86,39,0,-10,0,0,1,0},
+		off={39,55,39,0,-27,0,0,1,0},
 	}
 	unitCollisionVolume["packo"] = {
-		on={49,45,49,-0.5,-14,0,0,1,0},
-		off={49,26,49,-0.5,0,0,0,1,0},
+		on={49,51,49,-0.5,-10,0,0,1,0},
+		off={49,23,49,-0.5,-10,0,0,1,0},
+	}
+	unitCollisionVolume["shiva"] = {
+		on={54,50,50,0,2,-1,0,1,0},
+		off={54,45,50,0,-4,-1,0,1,0},
 	}
 
-return unitCollisionVolume
+	pieceCollisionVolume = {}
+	dynamicPieceCollisionVolume = {}
+	--[[ sample's from xta
+	pieceCollisionVolume["arm_big_bertha"] = {
+		["0"]={true,28,74,28,0,34,0,1,1},
+		["2"]={true,15,15,113,0,0,30,1,2},
+		["1"]={false},
+		["3"]={false},
+		["4"]={false},
+	}
+	
+	pieceCollisionVolume["arm_vulcan"] = {
+		["1"]={true,60,94,60,0,47,-1,1,1},
+		["6"]={true,36,36,106,0,0,27,1,2},
+		["0"]={false},
+		["2"]={false},
+		["3"]={false},
+		["5"]={false},
+		["4"]={false},
+		["7"]={false},
+		["8"]={false},
+		["9"]={false},
+		["10"]={false},
+		["11"]={false},
+		["12"]={false},
+		["13"]={false},
+		["14"]={false},
+	}
+
+	dynamicPieceCollisionVolume["core_viper"] = {
+		on = {
+			["0"]={true,51,12,53,0,4,0,2,0},
+			["5"]={true,25,66,25,0,-14,0,1,1},
+			["1"]={false},
+			["2"]={false},
+			["3"]={false},
+			["4"]={false},
+			["6"]={false},
+			["7"]={false},
+			["8"]={false},
+			["9"]={false},
+			["10"]={false},
+		},
+		off = {
+			["0"]={true,51,12,53,0,4,0,2,0},
+			["1"]={false},
+			["2"]={false},
+			["3"]={false},
+			["4"]={false},
+			["5"]={false},
+			["6"]={false},
+			["7"]={false},
+			["8"]={false},
+			["9"]={false},
+			["10"]={false},
+		}
+	}
+--]]
+return unitCollisionVolume, pieceCollisionVolume, dynamicPieceCollisionVolume
--- luarules/gadgets/unit_dynamic_collision_volume.lua
+++ luarules/gadgets/unit_dynamic_collision_volume.lua
@@ -10,45 +10,118 @@ function gadget:GetInfo()
   }
 end
 
-
+-- Pop-up style unit and per piece collision volume definitions
 local popupUnits = {}		--list of pop-up style units
-local unitCollisionVolume = include("LuaRules/Configs/CollisionVolumes.lua")	--pop-up style unit collision volume definitions
-	
+local unitCollisionVolume, pieceCollisionVolume, dynamicPieceCollisionVolume = include("LuaRules/Configs/CollisionVolumes.lua")
 
+-- Localization and speedups
+local spGetPieceCollisionData = Spring.GetUnitPieceCollisionVolumeData
+local spSetPieceCollisionData = Spring.SetUnitPieceCollisionVolumeData
+local spGetUnitCollisionData = Spring.GetUnitCollisionVolumeData
+local spSetUnitCollisionData = Spring.SetUnitCollisionVolumeData
+local spGetFeatureCollisionData = Spring.GetFeatureCollisionVolumeData
+local spSetFeatureCollisionData = Spring.SetFeatureCollisionVolumeData
+local spArmor = Spring.GetUnitArmored
+local spActive = Spring.GetUnitIsActive
+local airScalX, airScalY, airScalZ
+local pairs = pairs	
+
+function gadget:Initialize()
+	-- Spring 0.83 doesn't scale collision volumes of aircraft by additional 0.5 like Spring 0.82 does
+	if spGetFeatureCollisionData then
+		airScalX, airScalY, airScalZ = 0.375, 0.225, 0.45
+	else
+		airScalX, airScalY, airScalZ = 0.75, 0.45, 0.9
+	end
+--[[ Print all feature defs
+ for id,featureDef in pairs(FeatureDefs) do
+   for name,param in featureDef:pairs() do
+     Spring.Echo(name,param)
+   end
+ end
+]]--
+end
+
+	
 if (gadgetHandler:IsSyncedCode()) then
 
 	--Reduces the diameter of default (unspecified) collision volume for 3DO models,
 	--for S3O models it's not needed and will in fact result in wrong collision volume
+	--also handles per piece collision volume definitions
 	function gadget:UnitCreated(unitID, unitDefID, unitTeam)
-		if UnitDefs[unitDefID].model.type=="3do" then
-			local xs, ys, zs, xo, yo, zo, vtype, htype, axis, _ = Spring.GetUnitCollisionVolumeData(unitID)
-			if (vtype==4 and xs==ys and ys==zs) then
-				if (xs>47 and not UnitDefs[unitDefID].canFly) then
-					Spring.SetUnitCollisionVolumeData(unitID, xs*0.68, ys*0.68, zs*0.68,  xo, yo, zo,  vtype, htype, axis)
-				else
-					Spring.SetUnitCollisionVolumeData(unitID, xs*0.75, ys*0.75, zs*0.75,  xo, yo, zo,  vtype, htype, axis)
+		if (pieceCollisionVolume[UnitDefs[unitDefID].name]) then
+			--[[  ment for per piece collision volumes for XTA commanders 
+			if (UnitDefs[unitDefID].isCommander==true) then
+				local pieceList = Spring.GetUnitPieceList(unitID)
+				for i=0, #pieceList-1 do
+					local xs, ys, zs, xo, yo, zo, vtype, ttype, axis, en = spGetPieceCollisionData(unitID, i)
+					spSetPieceCollisionData(unitID, i, true, true,true,true, xs, ys, zs, xo, yo, zo, 1, 1)
 				end
-		    end
+			else
+			]]--
+				for pieceIndex, p in pairs(pieceCollisionVolume[UnitDefs[unitDefID].name]) do
+					if (p[1]==true) then
+						spSetPieceCollisionData(unitID, pieceIndex, p[1], p[1],p[1],p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
+					else
+						spSetPieceCollisionData(unitID, pieceIndex, false, false,false,false, 1, 1, 1, 0, 0, 0, 1, 1)
+					end
+				end
+			--end
+		elseif dynamicPieceCollisionVolume[UnitDefs[unitDefID].name] then
+			for pieceIndex, p in pairs(dynamicPieceCollisionVolume[UnitDefs[unitDefID].name].on) do
+				if (p[1]==true) then
+					spSetPieceCollisionData(unitID, pieceIndex, p[1], p[1],p[1],p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
+				else
+					spSetPieceCollisionData(unitID, pieceIndex, false, false,false,false, 1, 1, 1, 0, 0, 0, 1, 1)
+				end
+			end
+		else
+			if UnitDefs[unitDefID].model.type=="3do" then
+				local xs, ys, zs, xo, yo, zo, vtype, htype, axis, _ = spGetUnitCollisionData(unitID)
+				if (vtype==4 and xs==ys and ys==zs) then
+					if (xs>47 and not UnitDefs[unitDefID].canFly) then
+						spSetUnitCollisionData(unitID, xs*0.68, ys*0.68, zs*0.68,  xo, yo, zo,  vtype, htype, axis)
+					elseif (not UnitDefs[unitDefID].canFly) then
+						spSetUnitCollisionData(unitID, xs*0.75, ys*0.75, zs*0.75,  xo, yo, zo,  vtype, htype, axis)
+					else
+						spSetUnitCollisionData(unitID, xs*airScalX, ys*airScalY, zs*airScalZ,  xo, yo, zo,  vtype, htype, axis)
+					end
+				end
+			end
 		end
 	end
 
 
-	--[[ unsupported by engine ATM, same as for 3DO units, but for features
+	-- Requires Spring 0.83 and up, same as for 3DO units, but for features,
+	-- disabled on Spring <0.83
+	if spGetFeatureCollisionData then
 	function gadget:FeatureCreated(featureID, allyTeam)
-		local xs, ys, zs, xo, yo, zo, vtype, htype, axis, _ = Spring.GetFeatureCollisionVolumeData(featureID)
-		if (vtype==4 and xs==ys and ys==zs) then
-			Spring.SetFeatureCollisionVolumeData(unitID, xs*0.75, ys*0.75, zs*0.75,  xo, yo, zo,  vtype, htype, axis)
-
+		local featureModel = FeatureDefs[Spring.GetFeatureDefID(featureID)].modelname
+		featureModel = featureModel:lower()
+		if featureModel:find(".3do") then
+			local xs, ys, zs, xo, yo, zo, vtype, htype, axis, _ = spGetFeatureCollisionData(featureID)
+			if (vtype==4 and xs==ys and ys==zs) then
+				local yrs
+				if (xs>47) then
+					yrs = ys*0.60
+					spSetFeatureCollisionData(featureID, xs*0.68, yrs, zs*0.68,  xo, yo-yrs*0.15, zo,  vtype, htype, axis)
+				else
+					yrs = ys*0.67
+					spSetFeatureCollisionData(featureID, xs*0.75, yrs, zs*0.75,  xo, yo-yrs*0.15, zo,  vtype, htype, axis)
+				end
+			end
 		end
 	end
-	]]--
+	end
 
 
 	--check if a unit is pop-up type (the list must be entered manually)
 	function gadget:UnitFinished(unitID, unitDefID, unitTeam)
 		local un = UnitDefs[unitDefID].name
 		if unitCollisionVolume[un] then
-			popupUnits[unitID]={name=un, state=-1}
+			popupUnits[unitID]={name=un, state=-1, perPiece=false}
+		elseif dynamicPieceCollisionVolume[un] then
+			popupUnits[unitID]={name=un, state=-1, perPiece=true}
 		end
 	end
 
@@ -64,32 +137,57 @@ if (gadgetHandler:IsSyncedCode()) then
 	--Dynamic adjustment of pop-up style of units' collision volumes based on
 	--unit's ARMORED status, runs twice per second
 	function gadget:GameFrame(n)
-		if (n%20 ~= 0) then
+		if (n%15 ~= 0) then
 			return
 		end
 		local p
 		for unitID,defs in pairs(popupUnits) do
-			if Spring.GetUnitArmored(unitID) then
-				if (defs.state ~= 0) then
-					p = unitCollisionVolume[defs.name].off
-					Spring.SetUnitCollisionVolumeData(unitID, p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
-					popupUnits[unitID].state = 0
+			if spArmor(unitID) then
+				if defs.perPiece then
+					if (defs.state ~= 0) then
+						for pieceIndex, p in pairs(dynamicPieceCollisionVolume[defs.name].off) do
+							if (p[1]==true) then
+								spSetPieceCollisionData(unitID, pieceIndex, p[1], p[1],p[1],p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
+							else
+								spSetPieceCollisionData(unitID, pieceIndex, false, false,false,false, 1, 1, 1, 0, 0, 0, 1, 1)
+							end
+						end
+						popupUnits[unitID].state = 0
+					end
+				else
+					if (defs.state ~= 0) then
+						p = unitCollisionVolume[defs.name].off
+						spSetUnitCollisionData(unitID, p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
+						popupUnits[unitID].state = 0
+					end
 				end
 			else
-				if (defs.state ~= 1) then
-					p = unitCollisionVolume[defs.name].on
-					Spring.SetUnitCollisionVolumeData(unitID, p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
-					popupUnits[unitID].state = 1
+				if defs.perPiece then
+					if (defs.state ~= 1) then
+						for pieceIndex, p in pairs(dynamicPieceCollisionVolume[defs.name].on) do
+							if (p[1]==true) then
+								spSetPieceCollisionData(unitID, pieceIndex, p[1], p[1],p[1],p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
+							else
+								spSetPieceCollisionData(unitID, pieceIndex, false, false,false,false, 1, 1, 1, 0, 0, 0, 1, 1)
+							end
+						end
+						popupUnits[unitID].state = 1
+					end		
+				else
+					if (defs.state ~= 1) then
+						p = unitCollisionVolume[defs.name].on
+						spSetUnitCollisionData(unitID, p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
+						popupUnits[unitID].state = 1
+					end
 				end
-				--[[ if units doesn't use ARMORED variable, uncomment this block to make it use ACTIVATION as well
-				if ( Spring.GetUnitIsActive(unitID) ) then
+				--[[ if units don't use ARMORED variable, uncomment this block to make it use ACTIVATION as well
+				if ( spActive(unitID) ) then
 					p = unitCollisionVolume[name].on
 				else
 					p = unitCollisionVolume[name].off
 				end
 				]]--
-			end
-			
+			end			
 		end
 	end
 end
