378e1fd2b92bfbdeb07f83cd023716c1113c72c0
nixtux <davehomer@googlemail.com>
2011-12-24 08:01:57 -0800

    Bump version number

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
   name='Tech Annihilation',
   description='The New Generation Of War',
   shortname='TA',
-  version='v2.14-dev',
+  version='v2.15-dev',
   mutator='',
   game='',
   shortGame='',
