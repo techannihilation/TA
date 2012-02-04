8cf81ce498adfafc7736ccea816e9b714a193248
nixtux <davehomer@googlemail.com>
2012-02-04 23:13:18 +0000

    Add custom params for t3 evp units

--- units/corjeag.lua
+++ units/corjeag.lua
@@ -21,6 +21,9 @@ local unitDef = {
 	category = [[ALL MOBILE NOTDEFENSE NOTHOVERNOTVTOL NOTSUB NOTSUBNOTSHIP NOTVTOL SMALL WEAPON]],
 	copyright = [[Copyright 1997 Humongous Entertainment. All rights reserved.]],
 	corpse = [[dead]],
+	customparams = {
+		RequireTech = [[Advanced T1 Unit Research Centre]]
+		       },
 	defaultmissiontype = [[Standby]],
 	description = [[Anti-Air Vehicle]],
 	designation = [[COR-AAAV]],
