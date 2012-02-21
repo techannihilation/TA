53a4cea9f3dfd1eb8a3f195961a845e03c4219e7
nixtux <davehomer@googlemail.com>
2012-02-21 22:42:16 +0000

    Add new adv windmills

---
+++ units/armawin.lua
@@ -0,0 +1,112 @@
+-- UNITDEF -- ARMAWIN --
+--------------------------------------------------------------------------------
+
+local unitName = "armawin"
+
+--------------------------------------------------------------------------------
+
+local unitDef = {
+  acceleration       = 0,
+  activateWhenBuilt  = true,
+  bmcode             = 0,
+  brakeRate          = 0,
+  buildCostEnergy    = 200,
+  buildCostMetal     = 36,
+  builder            = false,
+  buildPic           = [[armawin.DDS]],
+  buildTime          = 1000,
+  category           = [[LEVEL1 NOTAIR ALL]],
+  corpse             = [[DEAD]],
+  description        = [[Produces Energy]],
+  energyStorage      = 0,
+  energyUse          = 0,
+  explodeAs          = [[SMALL_BUILDINGEX]],
+  footprintX         = 7,
+  footprintZ         = 7,
+  iconType           = [[building]],
+  idleAutoHeal       = 5,
+  idleTime           = 1800,
+  maxDamage          = 280,
+  maxSlope           = 10,
+  maxVelocity        = 0,
+  maxWaterDepth      = 0,
+  metalStorage       = 0,
+  name               = [[Advanced Wind Generator]],
+  noAutoFire         = false,
+  objectName         = [[ARMAWIN]],
+  seismicSignature   = 0,
+  selfDestructAs     = [[SMALL_BUILDING]],
+  side               = [[CORE]],
+  sightDistance      = 273,
+  smoothAnim         = true,
+  TEDClass           = [[ENERGY]],
+  turnRate           = 0,
+  unitname           = [[armawin]],
+  windGenerator      = 90,
+  workerTime         = 0,
+  yardMap            = [[ooooooooo]],
+  sounds = {
+    canceldestruct     = [[cancel2]],
+    underattack        = [[warning1]],
+    count = {
+      [[count6]],
+      [[count5]],
+      [[count4]],
+      [[count3]],
+      [[count2]],
+      [[count1]],
+    },
+    select = {
+      [[windgen1]],
+    },
+  },
+}
+
+
+--------------------------------------------------------------------------------
+
+local featureDefs = {
+  DEAD = {
+    blocking           = true,
+    category           = [[corpses]],
+    damage             = 206,
+    description        = [[Wreckage]],
+    energy             = 0,
+    featureDead        = [[HEAP]],
+    featurereclamate   = [[SMUDGE01]],
+    footprintX         = 7,
+    footprintZ         = 7,
+    height             = 40,
+    hitdensity         = 100,
+    metal              = 23,
+    object             = [[ARMAWIN_DEAD]],
+    reclaimable        = true,
+    seqnamereclamate   = [[TREE1RECLAMATE]],
+    world              = [[All Worlds]],
+  },
+  HEAP = {
+    blocking           = false,
+    category           = [[heaps]],
+    damage             = 101,
+    description        = [[Wreckage]],
+    energy             = 0,
+    featurereclamate   = [[SMUDGE01]],
+    footprintX         = 7,
+    footprintZ         = 7,
+    height             = 4,
+    hitdensity         = 100,
+    metal              = 9,
+    object             = [[7X7B]],
+    reclaimable        = true,
+    seqnamereclamate   = [[TREE1RECLAMATE]],
+    world              = [[All Worlds]],
+  },
+}
+unitDef.featureDefs = featureDefs
+
+
+--------------------------------------------------------------------------------
+
+return lowerkeys({ [unitName] = unitDef })
+
+--------------------------------------------------------------------------------
