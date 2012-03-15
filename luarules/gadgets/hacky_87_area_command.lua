914afc30dfe3e98e4409b5974fbedb5f64a92cbc
nixtux <davehomer@googlemail.com>
2012-03-15 18:49:09 +0000

    update area hax gadget to pako's version , thank you

--- luarules/gadgets/hacky_87_area_command.lua
+++ luarules/gadgets/hacky_87_area_command.lua
@@ -1,12 +1,12 @@
 function gadget:GetInfo()
   return {
-    name      = "Hacky 87.0 Area Command workaround",
-    desc      = "Uses double wait to fix area command halting",
-    author    = "Google Frog",
-    date      = "12 March 2012",
+    name      = "Hacky unreached Command workaround",
+    desc      = "Set smaller move goal for builder commands",
+    author    = "Pako",
+    date      = "15 March 2012",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = true  --  loaded by default?
+    enabled   = true
   }
 end
 
@@ -16,41 +16,45 @@ end
 if (not gadgetHandler:IsSyncedCode()) then
     return
 end
-
 -------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------
-
 local CMD_RECLAIM = CMD.RECLAIM
 local CMD_RESURRECT = CMD.RESURRECT
-local CMD_RESURRECT = CMD.REPAIR
-local CMD_WAIT = CMD.WAIT
-local spGiveOrderToUnit = Spring.GiveOrderToUnit
-local spValidUnitID = Spring.ValidUnitID
+local CMD_REPAIR = CMD.REPAIR
 
-local units = {count = 0, unitID = {}}
-local thereIsStuffToDo = false
+local builders = {}
 
-function gadget:UnitCmdDone(unitID, unitDefID, team, cmdID, cmdTag)
-	if (cmdID == CMD.RECLAIM or cmdID == CMD_RESURRECT or cmdID == CMD_REPAIR or cmdID < 0) then
-		-- Double wait requires a 1 frame delay
-		thereIsStuffToDo = true
-		units.count = units.count + 1
-		units.unitID[units.count] = unitID
-	end
+function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
+  if unitDefID and tonumber(UnitDefs[unitDefID].buildSpeed) and UnitDefs[unitDefID].buildSpeed > 0 then
+    builders[unitID] = true
+  else
+    builders[unitID] = nil
+  end
 end
-
 --buildDistance
 function gadget:GameFrame(f)
-	if thereIsStuffToDo then
-		for i = 1, units.count do
-			local unitID = units.unitID[i]
-			if Spring.ValidUnitID(unitID) then
-				-- Double wait, is there anything you can't fix? <3
-				Spring.GiveOrderToUnit(unitID,CMD_WAIT,{},{})
-				Spring.GiveOrderToUnit(unitID,CMD_WAIT,{},{})
-			end
+	if f%37 == 0 then
+	  local allUnits = Spring.GetAllUnits()
+	  for i=1,#allUnits,1 do
+	    local unitID = allUnits[i]
+	    if builders[unitID] then
+	      local fCmd = Spring.GetUnitCommands(unitID)[1]
+		if fCmd then  
+		  local cmdID = fCmd.id
+	      if (cmdID == CMD_RECLAIM or cmdID == CMD_RESURRECT or cmdID == CMD_REPAIR or (tonumber(cmdID) and cmdID < 0)) then
+		local ufID = fCmd.params[1]
+		local x,y,z
+		if Spring.ValidUnitID(ufID) then
+		  x,y,z = Spring.GetUnitPosition(ufID)
+		  elseif ufID and Spring.ValidFeatureID(ufID-Game.maxUnits) then
+		    x,y,z = Spring.GetFeaturePosition(ufID-Game.maxUnits)
 		end
-		thereIsStuffToDo = false
-		units = {count = 0, unitID = {}}
+		if x then
+		Spring.SetUnitMoveGoal(unitID,x,y,z,40)
+		end
+	      end
+		end
+	    end
+	  end
 	end
 end
