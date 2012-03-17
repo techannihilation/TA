d251c4860a0d9e568e9e2a86e2eee51d18353bf7
nixtux <davehomer@googlemail.com>
2012-03-17 21:30:35 +0000

    Update to pako's latest version

--- luarules/gadgets/hacky_87_area_command.lua
+++ luarules/gadgets/hacky_87_area_command.lua
@@ -1,6 +1,6 @@
 function gadget:GetInfo()
   return {
-    name      = "Hacky unreached Command workaround",
+    name      = "Hacky unreached Command workaround 4",
     desc      = "Set smaller move goal for builder commands",
     author    = "Pako",
     date      = "15 March 2012",
@@ -25,36 +25,47 @@ local CMD_REPAIR = CMD.REPAIR
 local builders = {}
 
 function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
-  if unitDefID and tonumber(UnitDefs[unitDefID].buildSpeed) and UnitDefs[unitDefID].buildSpeed > 0 then
-    builders[unitID] = true
+  if unitDefID and tonumber(UnitDefs[unitDefID].buildSpeed) and UnitDefs[unitDefID].buildSpeed > 0  and tonumber(UnitDefs[unitDefID].speed) and UnitDefs[unitDefID].speed>0 then
+    builders[unitID] = tonumber(UnitDefs[unitDefID].buildDistance) or nil
   else
     builders[unitID] = nil
   end
 end
---buildDistance
+
+function gadget:UnitDestroyed(unitID, unitDefID, unitTeam)
+  builders[unitID] = nil
+end
+
+local function distance(x1,z1,x2,z2)
+  local dx,dz = x1-x2,z1-z2
+  return ((dx*dx)+(dz*dz))^0.5
+end
+
 function gadget:GameFrame(f)
-	if f%37 == 0 then
-	  local allUnits = Spring.GetAllUnits()
-	  for i=1,#allUnits,1 do
-	    local unitID = allUnits[i]
-	    if builders[unitID] then
-	      local fCmd = Spring.GetUnitCommands(unitID)[1]
-		if fCmd then  
+  local framemod=f%37
+	  for unitID,buildDistance in pairs(builders) do
+	    if unitID%37 == framemod then
+	     local fCmd = Spring.GetUnitCommands(unitID)[1]
+	     if fCmd then
 		  local cmdID = fCmd.id
 	      if (cmdID == CMD_RECLAIM or cmdID == CMD_RESURRECT or cmdID == CMD_REPAIR or (tonumber(cmdID) and cmdID < 0)) then
 		local ufID = fCmd.params[1]
-		local x,y,z
+		local x,y,z,radius
+		radius = 18
 		if Spring.ValidUnitID(ufID) then
-		  x,y,z = Spring.GetUnitPosition(ufID)
-		  elseif ufID and Spring.ValidFeatureID(ufID-Game.maxUnits) then
+		    x,y,z = Spring.GetUnitPosition(ufID)
+		    radius = Spring.GetUnitRadius(ufID)
+		 elseif ufID and Spring.ValidFeatureID(ufID-Game.maxUnits) then
 		    x,y,z = Spring.GetFeaturePosition(ufID-Game.maxUnits)
 		end
 		if x then
-		Spring.SetUnitMoveGoal(unitID,x,y,z,40)
+		  local xb,_,zb = Spring.GetUnitPosition(unitID)
+		  if buildDistance < (distance(x,z,xb,zb)-radius) then --TODO radius
+		    Spring.SetUnitMoveGoal(unitID,x,y,z,18)
+		  end
 		end
 	      end
-		end
+	     end
 	    end
 	  end
-	end
 end
