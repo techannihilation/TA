c063b41b68d4f85648b75b1bf825d5f9bb29434e
nixtux <davehomer@googlemail.com>
2012-03-17 22:26:27 +0000

    Fix typo , all tag are lowercase

--- gamedata/unitdefs_post.lua
+++ gamedata/unitdefs_post.lua
@@ -316,12 +316,12 @@ for name, ud in pairs(UnitDefs) do
 	if (ud.maxvelocity) then 
 		ud.turninplacespeedlimit = ud.maxvelocity or 0
 	end
-	if ud.movementClass and (ud.movementClass:find("TANK",1,true) or ud.movementClass:find("HOVER",1,true)) then
+	if ud.movementclass and (ud.movementclass:find("TANK",1,true) or ud.movementclass:find("HOVER",1,true)) then
 		if (ud.maxvelocity) then 
 			ud.turninplace = 0
 			ud.turninplacespeedlimit = (ud.maxvelocity/2) or 0
 		end
-	elseif ud.movementClass and (ud.movementClass:find("KBOT",1,true)) then
+	elseif ud.movementclass and (ud.movementclass:find("KBOT",1,true)) then
 		if (ud.maxvelocity) and (ud.turninplace) then 
 			ud.turninplaceanglelimit = 91
 		end
