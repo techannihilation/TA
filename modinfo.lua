7341ac3e21998be1ea9867c4d3b513184cf9be05
nixtux <davehomer@googlemail.com>
2012-06-15 21:11:32 +0000

    Update version string

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
   name='Tech Annihilation',
   description='The New Generation Of War',
   shortname='TA',
-  version='v2.18',
+  version='v2.19-dev',
   mutator='',
   game='',
   shortGame='',
