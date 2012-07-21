19f27aa026fa4cbba321505ead2d251b29f2d3ef
nixtux <davehomer@googlemail.com>
2012-07-21 08:13:19 +0000

    Update Group label widget

--- luaui/widgets/gui_group_label.lua
+++ luaui/widgets/gui_group_label.lua
@@ -19,10 +19,20 @@ function widget:GetInfo()
     date      = "June 12, 2008",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = false  --  loaded by default?
+    enabled   = true  --  loaded by default?
   }
 end
 
+local glPushMatrix				= gl.PushMatrix
+local glTranslate				= gl.Translate
+local glPopMatrix				= gl.PopMatrix
+local glColor					= gl.Color
+local glText					= gl.Text
+local glBillboard				= gl.Billboard
+
+local spGetGroupList			= Spring.GetGroupList
+local spGetGroupUnits			= Spring.GetGroupUnits
+local spGetUnitViewPosition 	= Spring.GetUnitViewPosition
 local textColor = {0.7, 1.0, 0.7, 1.0}
 local textSize = 12.0
 
@@ -33,18 +43,18 @@ local textSize = 12.0
 --
 
 function widget:DrawWorld()
-   local groups = Spring.GetGroupList()
+   local groups = spGetGroupList()
    for group, _ in pairs(groups) do
-      units = Spring.GetGroupUnits(group)
+      units = spGetGroupUnits(group)
       for _, unit in ipairs(units) do
          if Spring.IsUnitInView(unit) then
-            local ux, uy, uz = Spring.GetUnitViewPosition(unit)
-            gl.PushMatrix()
-            gl.Translate(ux, uy, uz)
-            gl.Billboard()
-            gl.Color(textColor)
-            gl.Text("" .. group, -10.0, -15.0, textSize, "cn")
-            gl.PopMatrix()
+            local ux, uy, uz = spGetUnitViewPosition(unit)
+            glPushMatrix()
+            glTranslate(ux, uy, uz)
+            glBillboard()
+            glColor(textColor)
+            glText("" .. group, -10.0, -15.0, textSize, "cn")
+            glPopMatrix()
          end
       end
    end
