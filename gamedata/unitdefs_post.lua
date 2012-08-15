ee29f00feab1c9027682587a4ca059a4a7a59638
nixtux <davehomer@googlemail.com>
2012-08-15 19:17:43 +0000

    Dont forget tllcom

--- gamedata/unitdefs_post.lua
+++ gamedata/unitdefs_post.lua
@@ -218,7 +218,7 @@ if (Spring.GetModOptions) then
 	
    if (modOptions.mo_storageowner == "com") then
     for name, ud in pairs(UnitDefs) do  
-     if (name == "armcom" or name == "corcom") then
+     if (name == "armcom" or name == "corcom" or name =="tllcom") then
       ud.energyStorage = modOptions.startenergy or 1000
       ud.metalStorage = modOptions.startmetal or 1000
      end
