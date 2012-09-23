cbd32f61c12c2ad841c7f65ba380b0305beba883
nixtux <davehomer@googlemail.com>
2012-09-23 20:20:17 +0000

    Update unit crashing aircraft -- neg xp bug

--- luarules/gadgets/unit_crashing_aircraft.lua
+++ luarules/gadgets/unit_crashing_aircraft.lua
@@ -17,14 +17,37 @@ local GetUnitHealth 	= Spring.GetUnitHealth
 local random			= math.random 
 local SetUnitCOBValue 	= Spring.SetUnitCOBValue
 local SetUnitNoSelect	= Spring.SetUnitNoSelect
+local SetUnitCosts		= Spring.SetUnitCosts
+local crashing={}
 
 function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, attackerID, attackerDefID, attackerTeam)
+	if crashing[unitID] then 
+		return 0,0
+	end --hacky
 	if UnitDefs[unitDefID]["canFly"] == true and (damage>GetUnitHealth(unitID)) and random()>0.5 then
 	--NOTE: strafe airmovetype aircraft DO NOT CRASH, only regular stuff like bombers
-		--Spring.Echo('CRASHING AIRCRAFT')
-		Spring.SetUnitCosts(unitID, {metalCost  = 1, energyCost = 1})
+		--Spring.Echo('CRASHING AIRCRAFT',unitID)
 		SetUnitCOBValue(unitID, COB.CRASHING, 1)
-		Spring.SetUnitNoSelect(unitID,true) --cause setting to neutral still allows selection (wtf?)
+		--SetUnitCosts(unitID,{10000,0,0}) this doesnt work either :)
+		SetUnitNoSelect(unitID,true) --cause setting to neutral still allows selection (wtf?)
+		crashing[unitID]=true
+		--return 0,0--TEST THIS
+	end
+	return damage,1
+end
+
+function gadget:UnitDestroyed(unitID, unitDefID, teamID, attackerID, attackerDefID, attackerTeamID)
+	if crashing[unitID] then
+		--Spring.Echo('CRASHING AIRCRAFT UNITDESTROYED CALLED!',unitID)
+		crashing[unitID]=nil
 	end
 end
 
+function gadget:AllowUnitBuildStep(builderID, builderTeamID, uID, uDefID, step) --THIS IS VERY HACKY AND BAD FOR PERFORMACE!
+	--Spring.Echo('AllowUnitBuildStep',uID,step)
+	if step<0 and crashing[uID] then
+		--Spring.Echo('AllowUnitBuildStep ON CRASHING!')
+		return false
+	end
+	return true
+end
