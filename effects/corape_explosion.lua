47ee552880fd7a17552bc84bbacec277ddb0c71b
nixtux <davehomer@googlemail.com>
2012-10-21 19:07:22 +0000

    Add new explosion for corape

---
+++ effects/corape_explosion.lua
@@ -0,0 +1,97 @@
+-- corape_explosion
+
+return {
+  ["corape_explosion"] = {
+ usedefaultexplosions = false,
+     
+       groundflash = {
+      circlealpha        = 0.25,
+      circlegrowth       = 0.001,
+      flashalpha         = 0.82,
+      flashsize          = 76,
+      ttl                = 16,
+      color = {
+        [1]  = 0.99599999189377,
+        [2]  = 0.23000001907349,
+        [3]  = 0.21599998474121,
+      },
+    },
+ foom = {
+      air                = true,
+      class              = [[heatcloud]],
+      count              = 1,
+      ground             = true,
+      unit               = 1,
+      properties = {
+        heat               = 10,
+        heatfalloff        = 0.5,
+        maxheat            = 15,
+        pos                = [[dir]],
+        size               = 7,
+        sizegrowth         = 0.7,
+        speed              = [[dir]],
+        texture            = [[explo]],
+      },
+    },
+  
+     smoke = {
+      air                = true,
+      class              = [[CSimpleParticleSystem]],
+      count              = 1,
+      ground             = true,
+      water              = true,
+      properties = {
+        airdrag            = 0.95,
+        colormap           = [[1 0.6 0.4 .01  0.6 0.2 0 0.8	0.1 0.1 0.1 0.8 0 0 0 0.01]],
+        directional        = false,
+        emitrot            = 80,
+        emitrotspread      = 8,
+        emitvector         = [[dir]],
+        gravity            = [[0 r0.1, .02, 0]],
+        numparticles       = 25,
+        particlelife       = 18,
+        particlelifespread = 68,
+        particlesize       = 7,
+        particlesizespread = 25,
+        particlespeed      = 0.90,
+        particlespeedspread = 1.6,
+        pos                = [[0, 0, 0]],
+        sizegrowth         = -0.46,
+        sizemod            = 1.0,
+        texture            = [[GenericSmokeCloud1]],
+      },
+    },
+    
+      spark = {
+      air                = false,
+      class              = [[CSimpleParticleSystem]],
+      count              = 1,
+      ground             = false,
+      unit               = 1,
+      water              = false,
+      properties = {
+        airdrag            = 1,
+        colormap           = [[0.9 0.7 0.5 .01   0.9 0.7 0.5 .01   	0 0 0 0.01]],
+        directional        = true,
+        emitrot            = 0,
+        emitrotspread      = [[0 r-360 r360]],
+        emitvector         = [[0, 1, 0]],
+        gravity            = [[0, -.15 r0.15 r-1, 0]],
+        numparticles       = 10,
+        particlelife       = 5,
+        particlelifespread = 4,
+        particlesize       = 6,
+        particlesizespread = 4,
+        particlespeed      = 4,
+        particlespeedspread = 3,
+        pos                = [[0, 0, 0]],
+        sizegrowth         = [[0.0 r .05]],
+        sizemod            = -1.0,
+        texture            = [[Plasma]],
+        useairlos          = true,
+      },
+    },
+  },
+
+}
+
