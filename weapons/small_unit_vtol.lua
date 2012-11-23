eb08e469428ac7d1c182b576abf4c4ff8d7d375b
nixtux <davehomer@googlemail.com>
2012-11-23 11:46:13 +0000

    Lower damage cause by selfd-ing vtol units

---
+++ weapons/small_unit_vtol.lua
@@ -0,0 +1,32 @@
+-- WEAPONDEF -- SMALL_UNIT_VTOL --
+--------------------------------------------------------------------------------
+
+local weaponName = "small_unit_vtol"
+
+--------------------------------------------------------------------------------
+
+local weaponDef = {
+	areaofeffect = 60,
+	ballistic = 1,
+	craterboost = 0,
+	cratermult = 0,
+	explosiongenerator = [[custom:FLASHSMALLUNIT]],
+	impulseboost = 0.12300000339746,
+	impulsefactor = 0.12300000339746,
+	name = [[Small vtol]],
+	range = 480,
+	reloadtime = 3.5999999046326,
+	rendertype = 4,
+	soundhit = [[xplomed2]],
+	soundstart = [[largegun]],
+	turret = 1,
+	weaponvelocity = 250,
+	damage = {
+		default = 30,
+	},
+}
+--------------------------------------------------------------------------------
+
+return lowerkeys({[weaponName] = weaponDef})
+
+--------------------------------------------------------------------------------
