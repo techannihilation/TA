44a1826a34f71fe19bf4907043504db7f07d7c6d
nixtux <davehomer@googlemail.com>
2013-01-01 22:45:51 +0000

    cleanup

--- luarules/gadgets/Better_Ranged_Counted_Multi_Tech.lua
+++ luarules/gadgets/Better_Ranged_Counted_Multi_Tech.lua
@@ -935,8 +935,8 @@ if (gadgetHandler:IsSyncedCode()) then
 		end
 		local ICanHaz = CheckCmd(cmd,team,x,y,z)
 		if not ICanHaz then
-			Spring.PlaySoundFile("sounds/moarpower.wav", 5, x, y, z)
-			Spring.SpawnCEG("moarpower", x, y, z)
+			--Spring.PlaySoundFile("sounds/moarpower.wav", 5, x, y, z)
+			--Spring.SpawnCEG("moarpower", x, y, z)
 		end
 	return ICanHaz
 	end
@@ -950,7 +950,7 @@ if (gadgetHandler:IsSyncedCode()) then
             CanIHaz =  CheckCmd(-ud,team,builder)
         end
         if not CanIHaz then
-            Spring.PlaySoundFile("sounds/moarpower.wav", 5, x, y, z)
+            --Spring.PlaySoundFile("sounds/moarpower.wav", 5, x, y, z)
         end
         return CanIHaz
 	end
