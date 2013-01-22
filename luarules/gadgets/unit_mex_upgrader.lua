d073e1924b07b19d7e4519ce374b690117f5b1f3
nixtux <davehomer@googlemail.com>
2013-01-22 20:22:59 +0000

    Revert "Swap isshield for weapontype == shield"
    This reverts commit 11f4e9f89bd1c48426eab44faf8361564eb99f71.

--- luarules/gadgets/unit_mex_upgrader.lua
+++ luarules/gadgets/unit_mex_upgrader.lua
@@ -95,7 +95,7 @@ function determine(ud, wd)
         local mexDef = {} 
         mexDef.extractsMetal = extractsMetal 
         if #unitDef.weapons <= 1 then
-          if (#unitDef.weapons == 1 and wd[unitDef.weapons[1].weaponDef].weaponType == "Shield") then
+          if (#unitDef.weapons == 1 and wd[unitDef.weapons[1].weaponDef].isShield) then
 	    mexDef.armed = #unitDef.weapons < 0
           else
             mexDef.armed = #unitDef.weapons > 0      
