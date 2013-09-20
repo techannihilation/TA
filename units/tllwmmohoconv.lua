b51cd62d5a40099c148c466cb141d8910961e374
nixtux <davehomer@googlemail.com>
2013-09-20 20:36:35 +0100

    Add new floating moho MM for tll, need balancing

---
+++ units/tllwmmohoconv.lua
@@ -0,0 +1,87 @@
+-- UNITDEF -- TLLWMMOHOCONV --
+--------------------------------------------------------------------------------
+
+local unitName = "tllwmmohoconv"
+
+--------------------------------------------------------------------------------
+
+local unitDef = {
+	activateWhenBuilt = true,
+	bmcode = 0,
+	buildAngle = 8192,
+	buildCostEnergy = 274,
+	buildCostMetal = 27,
+	builder = false,
+	buildTime = 2200,
+	category = [[ALL NOTDEFENSE NOTHOVERNOTVTOL NOTMOBILE NOTSUB NOTSUBNOTSHIP NOTVTOL NOTWEAPON]],
+	corpse = [[dead]],
+	description = [[Converts upto 100 Energy into Metal]],
+	designation = [[WM-CONV]],
+	explodeAs = [[SMALL_BUILDINGEX]],
+	footprintX = 5,
+	footprintZ = 5,
+	maxDamage = 120,
+	maxSlope = 10,
+	minWaterDepth = 10,
+	name = [[Metal Producer]],
+	noAutoFire = false,
+	noChaseCategory = [[ALL]],
+	noshadow = 1,
+	objectName = [[tllwmmohoconv]],
+	onoffable = true,
+	ovradjust = 1,
+	selfDestructAs = [[SMALL_BUILDING]],
+	side = [[TLL]],
+	sightDistance = 200,
+	unitname = [[tllwmmohoconv]],
+	unitnumber = 870,
+	waterline = 0,
+	yardMap = [[wwwww wwwww wwwww wwwww wwwww]],
+	featureDefs = nil,
+	sounds = {
+		activate = [[metlon1]],
+		canceldestruct = [[cancel2]],
+		deactivate = [[metloff1]],
+		underattack = [[warning1]],
+		working = [[metlrun1]],
+		count = {
+			[1] = [[count6]],
+			[2] = [[count5]],
+			[3] = [[count4]],
+			[4] = [[count3]],
+			[5] = [[count2]],
+			[6] = [[count1]],
+		},
+		select = {
+			[1] = [[metlon1]],
+		},
+	},
+}
+
+--------------------------------------------------------------------------------
+
+local featureDefs = {
+	dead = {
+		blocking = true,
+		category = [[tll_corpses]],
+		damage = 0.6000 * unitDef.maxDamage,
+		description = [[Wreckage]],
+		featurereclamate = [[smudge01]],
+		footprintX = 3,
+		footprintZ = 3,
+		height = 12,
+		hitdensity = 102,
+		metal = 0.8000 * unitDef.buildCostMetal,
+		object = [[tllWMconv_dead]],
+		reclaimable = true,
+		seqnamereclamate = [[tree1reclamate]],
+		world = [[all]],
+	},
+}
+unitDef.featureDefs = featureDefs
+
+--------------------------------------------------------------------------------
+
+return lowerkeys({[unitName] = unitDef})
+
+--------------------------------------------------------------------------------
