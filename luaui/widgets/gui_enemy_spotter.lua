5b95b2cd59f8ba705bd2a98da52b4f0ac4e0401f
nixtux <davehomer@googlemail.com>
2014-01-31 23:09:40 +0000

    Add old version back

--- luaui/widgets/gui_enemy_spotter.lua
+++ luaui/widgets/gui_enemy_spotter.lua
@@ -13,10 +13,10 @@
 
 function widget:GetInfo()
    return {
-      name      = "EnemySpotter2",
+      name      = "EnemySpotter",
       desc      = "Draws transparant smoothed donuts under enemy units (with teamcolors or predefined colors, depending on situation)",
-      author    = "TradeMark  (Floris: added multiple ally color support + performance)",
-      date      = "22,Jan,2014",
+      author    = "TradeMark  (Floris: added multiple ally color support)",
+      date      = "17.03.2013",
       license   = "GNU GPL, v2 or later",
       layer     = 5,
       enabled   = false  --  loaded by default?
@@ -34,7 +34,7 @@ local skipOwnAllyTeam                   = true    -- keep this 'true' if you don
 
 local circleSize                        = 1
 local circleDivs                        = 12      -- how precise circle? octagon by default
-local circleOpacity                     = 0.15
+local circleOpacity                     = 0.13
 local innerSize                         = 1.35    -- circle scale compared to unit radius
 local outerSize                         = 1.30    -- outer fade size compared to circle scale (1 = no outer fade)
                                         
@@ -51,9 +51,28 @@ local EsMaxDist = 5500000 -- max dist at which to draw ETA
 
 -- Automatically generated local definitions
 
+local GL_ONE                  = GL.ONE
+local GL_ONE_MINUS_SRC_ALPHA  = GL.ONE_MINUS_SRC_ALPHA
+local GL_SRC_ALPHA            = GL.SRC_ALPHA
+local glBlending              = gl.Blending
+local glBeginEnd              = gl.BeginEnd
+local glColor                 = gl.Color
+local glCreateList            = gl.CreateList
+local glDeleteList            = gl.DeleteList
+local glDepthTest             = gl.DepthTest
 local glDrawListAtUnit        = gl.DrawListAtUnit
-
-local gaiaTeamID              = Spring.GetGaiaTeamID()
+local glPolygonOffset         = gl.PolygonOffset
+local glVertex                = gl.Vertex
+local spGetTeamColor          = Spring.GetTeamColor
+local spGetUnitDefDimensions  = Spring.GetUnitDefDimensions
+local spGetUnitDefID          = Spring.GetUnitDefID
+local spIsUnitSelected        = Spring.IsUnitSelected
+local spGetAllyTeamList       = Spring.GetAllyTeamList 
+local spGetTeamList           = Spring.GetTeamList
+local spGetVisibleUnits       = Spring.GetVisibleUnits
+local spIsGUIHidden           = Spring.IsGUIHidden
+local spGetUnitAllyTeam       = Spring.GetUnitAllyTeam
+                              
 local myTeamID                = Spring.GetLocalTeamID()
 local myAllyID                = Spring.GetMyAllyTeamID()
 
@@ -70,83 +89,81 @@ local pickTeamColor           = false
 --------------------------------------------------------------------------------
 
 
-local function drawSpottersBaseInner()
-   radstep = (2.0 * math.pi) / circleDivs
-   for i = 1, circleDivs do
-       a1 = (i * radstep)
-       a2 = ((i+1) * radstep)
-       gl.Color(usedSpotterColor[1], usedSpotterColor[2], usedSpotterColor[3], 0)
-       gl.Vertex(0, 0, 0)
-       gl.Color(usedSpotterColor[1], usedSpotterColor[2], usedSpotterColor[3], circleOpacity)
-       gl.Vertex(math.sin(a1), 0, math.cos(a1))
-       gl.Vertex(math.sin(a2), 0, math.cos(a2))
-   end
-end
-
-local function drawSpottersBaseOuter()
-   radstep = (2.0 * math.pi) / circleDivs
-   for i = 1, circleDivs do
-       a1 = (i * radstep)
-       a2 = ((i+1) * radstep)
-       gl.Color(usedSpotterColor[1], usedSpotterColor[2], usedSpotterColor[3], circleOpacity)
-       gl.Vertex(math.sin(a1), 0, math.cos(a1))
-       gl.Vertex(math.sin(a2), 0, math.cos(a2))
-       gl.Color(usedSpotterColor[1], usedSpotterColor[2], usedSpotterColor[3], 0)
-       gl.Vertex(math.sin(a2)*outerSize, 0, math.cos(a2)*outerSize)
-       gl.Vertex(math.sin(a1)*outerSize, 0, math.cos(a1)*outerSize)
-   end
-end
-
-local function drawSpotters()
-   gl.BeginEnd(GL.TRIANGLES, drawSpottersBaseInner)
-   if outerSize ~= 1 then
-       gl.BeginEnd(GL.QUADS, drawSpottersBaseOuter)
-   end
-end
-
-
-
 -- Creating polygons:
 function widget:Initialize()
-   allyTeamList = Spring.GetAllyTeamList()
-   numberOfAllyTeams = #allyTeamList
-   for allyTeamListIndex=1, numberOfAllyTeams do
-      allyID = allyTeamList[allyTeamListIndex]
+   local allyTeamList = spGetAllyTeamList()
+   local numberOfAllyTeams = #allyTeamList
+   for allyTeamListIndex = 1, numberOfAllyTeams do
+      local allyID                = allyTeamList[allyTeamListIndex]
       if not skipOwnAllyTeam  or  (skipOwnAllyTeam  and  not (allyID == myAllyID))  then
          allyToSpotterColorCount     = allyToSpotterColorCount+1
          allyToSpotterColor[allyID]  = allyToSpotterColorCount
-         usedSpotterColor      = spotterColor[allyToSpotterColorCount]
+         local usedSpotterColor      = spotterColor[allyToSpotterColorCount]
          if defaultColorsForAllyTeams < numberOfAllyTeams-1 then
-            teamList = Spring.GetTeamList(allyID)
-            numberOfTeamList = #teamList
-            for teamListIndex=1, numberOfTeamList do
-               teamID = teamList[teamListIndex]
-               if teamID ~= gaiaTeamID then
-                  if (teamListIndex == 1  and  #teamList <= keepTeamColorsForSmallAllyTeam) then     -- only check for the first allyTeam  (to be consistent with picking a teamcolor or default color, inconsistency could happen with different teamsizes)
-                     pickTeamColor = true
-                  end
-                  if pickTeamColor then
-                     -- pick the first team in the allyTeam and take the color from that one
-                     if (teamListIndex == 1) then
-                        r,g,b,a       = Spring.GetTeamColor(teamID)
-                        usedSpotterColor[1] = r
-                        usedSpotterColor[2] = g
-                        usedSpotterColor[3] = b
-                     end
+            local teamList              = spGetTeamList(allyID)
+            for teamListIndex = 1, #teamList do
+               local teamID = teamList[teamListIndex]
+               if (teamListIndex == 1  and  #teamList <= keepTeamColorsForSmallAllyTeam) then     -- only check for the first allyTeam  (to be consistent with picking a teamcolor or default color, inconsistency could happen with different teamsizes)
+                  pickTeamColor = true
+               end
+               if pickTeamColor then
+                  -- pick the first team in the allyTeam and take the color from that one
+                  if (teamListIndex == 1) then
+                     local r,g,b,a       = spGetTeamColor(teamID)
+                     usedSpotterColor[1] = r
+                     usedSpotterColor[2] = g
+                     usedSpotterColor[3] = b
                   end
                end
             end
          end
-         circlePolys[allyID] = gl.CreateList(drawSpotters)
+         
+         
+         circlePolys[allyID] = glCreateList(function()
+         
+            glBlending(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)      -- disable layer blending
+            
+            -- colored inner circle:
+            glBeginEnd(GL.TRIANGLES, function()
+               local radstep = (2.0 * math.pi) / circleDivs
+               for i = 1, circleDivs do
+                  local a1 = (i * radstep)
+                  local a2 = ((i+1) * radstep)
+                  --(fadefrom)
+                  glColor(usedSpotterColor[1], usedSpotterColor[2], usedSpotterColor[3], 0)
+                  glVertex(0, 0, 0)
+                  --(colorSet)
+                  glColor(usedSpotterColor[1], usedSpotterColor[2], usedSpotterColor[3], circleOpacity)
+                  glVertex(math.sin(a1), 0, math.cos(a1))
+                  glVertex(math.sin(a2), 0, math.cos(a2))
+               end
+            end)
+            
+            if (outerSize ~= 1) then
+               -- colored outer circle:
+               glBeginEnd(GL.QUADS, function()
+                  local radstep = (2.0 * math.pi) / circleDivs
+                  for i = 1, circleDivs do
+                     local a1 = (i * radstep)
+                     local a2 = ((i+1) * radstep)
+                     --(colorSet)
+                     glColor(usedSpotterColor[1], usedSpotterColor[2], usedSpotterColor[3], circleOpacity)
+                     glVertex(math.sin(a1), 0, math.cos(a1))
+                     glVertex(math.sin(a2), 0, math.cos(a2))
+                     --(fadeto)
+                     glColor(usedSpotterColor[1], usedSpotterColor[2], usedSpotterColor[3], 0)
+                     glVertex(math.sin(a2)*outerSize, 0, math.cos(a2)*outerSize)
+                     glVertex(math.sin(a1)*outerSize, 0, math.cos(a1)*outerSize)
+                  end
+               end)
+            end
+         end)
       end
    end
 end
 
-
 function widget:Shutdown()
-    for _,circlePoly in pairs(circlePolys) do
-        gl.DeleteList(circlePoly)
-    end
+   --glDeleteList(circlePolys)
 end
 
 
@@ -156,16 +173,16 @@ end
 
 -- Retrieving radius:
 local function GetUnitDefRealRadius(udid)
-   radius = realRadii[udid]
+   local radius = realRadii[udid]
    if (radius) then return radius end
-   ud = UnitDefs[udid]
+   local ud = UnitDefs[udid]
    if (ud == nil) then return nil end
-   dims = Spring.GetUnitDefDimensions(udid)
+   local dims = spGetUnitDefDimensions(udid)
    if (dims == nil) then return nil end
-   scale = ud.hitSphereScale -- missing in 0.76b1+
+   local scale = ud.hitSphereScale -- missing in 0.76b1+
    scale = ((scale == nil) or (scale == 0.0)) and 1.0 or scale
    radius = dims.radius / scale
-   realRadii[udid] = radius
+   realRadii[udid] = radius*circleSize
    return radius
 end
 
@@ -174,41 +191,39 @@ end
 --------------------------------------------------------------------------------
 
 
+-- Drawing:
 function widget:DrawWorldPreUnit()
    if not drawWithHiddenGUI then
-      if Spring.IsGUIHidden() then return end
+      if spIsGUIHidden() then return end
    end
-   visibleUnits = Spring.GetVisibleUnits()
-   numberOfVisibleUnits = #visibleUnits
-   if numberOfVisibleUnits then
-      gl.DepthTest(true)
-      gl.PolygonOffset(-100, -2)         -- else the spotters flicker
-      gl.Blending(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA)      -- disable layer blending
-      for i=1, numberOfVisibleUnits do
-        unitID = visibleUnits[i]
-	local cx, cy, cz = Spring.GetCameraPosition()
-        local ux,uy,uz = Spring.GetUnitViewPosition(unitID)
-          if ux~=nil then
-          local dx, dy, dz = ux-cx, uy-cy, uz-cz
-          local dist = dx*dx + dy*dy + dz*dz
-	    if dist < EsMaxDist then 
-            allyID = Spring.GetUnitAllyTeam(unitID)
-              if circlePolys[allyID] ~= nil then
-                if not skipOwnAllyTeam  or  (skipOwnAllyTeam  and  not (allyID == myAllyID))  then
-                unitDefIDValue = Spring.GetUnitDefID(unitID)
-                  if (unitDefIDValue) then
-                  radius = GetUnitDefRealRadius(unitDefIDValue) * circleSize
-                    if (radius) then
-                    glDrawListAtUnit(unitID, circlePolys[allyID], false, radius, 1.0, radius)
-                    end
+   glDepthTest(true)
+   glPolygonOffset(-100, -2)
+   local visibleUnits = spGetVisibleUnits()
+   if #visibleUnits then
+    for i=1, #visibleUnits do
+    unitID = visibleUnits[i]
+    local cx, cy, cz = Spring.GetCameraPosition()
+    local ux,uy,uz = Spring.GetUnitViewPosition(unitID)
+     if ux~=nil then
+       local dx, dy, dz = ux-cx, uy-cy, uz-cz
+       local dist = dx*dx + dy*dy + dz*dz
+	if dist < EsMaxDist then 
+         local allyID = spGetUnitAllyTeam(unitID)
+         if circlePolys[allyID] ~= nil then
+            if not skipOwnAllyTeam  or  (skipOwnAllyTeam  and  not (allyID == myAllyID))  then
+               local unitDefIDValue = spGetUnitDefID(unitID)
+               if (unitDefIDValue) then
+                  local radius = GetUnitDefRealRadius(unitDefIDValue) * circleSize
+                  if (radius) then
+                     glDrawListAtUnit(unitID, circlePolys[allyID], false, radius, 1.0, radius)
                   end
-                end
-              end
+               end
             end
-          end
-      gl.DepthTest(false)
+         end
+        end
+      end
+     end
    end
-end
 end
 
              
