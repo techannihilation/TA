06f14144969675ef354ce6dfcc2d3003fa59b848
nixtux <davehomer@googlemail.com>
2014-02-02 08:29:54 +0000

    Tech lock Behegoli, Increase Destruitor size by 25%, increase hp 1950>2750 and lock both units by t2.5RC, Reorganize t2 hover labs build menus

--- units/nsacbehe.lua
+++ units/nsacbehe.lua
@@ -60,6 +60,9 @@ local unitDef = {
 	unitnumber = 1408,
 	workerTime = 0,
 	featureDefs = nil,
+	customparams = {
+		RequireTech = [[Advanced T2 Unit Research Centre]],
+	},
 	sounds = {
 		canceldestruct = [[cancel2]],
 		underattack = [[warning1]],
