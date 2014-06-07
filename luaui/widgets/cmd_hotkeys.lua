7139fe473c2e3f7a8b898290dcd1c9e1c1dcc31e
nixtux <davehomer@googlemail.com>
2014-06-07 07:06:34 +0100

    Had hotkey for jump command, o

--- luaui/widgets/cmd_hotkeys.lua
+++ luaui/widgets/cmd_hotkeys.lua
@@ -1,7 +1,7 @@
 function widget:GetInfo()
 	return {
-		name = "Set Target Hotkeys",
-		desc = "Binds y for Set Target and j for Cancel Target" ,
+		name = "Set Hotkeys",
+		desc = "Binds y for Set Target and j for Cancel Target and o for Jump" ,
 		author = "Bluestone",
 		date = "14/05/2013",
 		license = "Horses",
@@ -13,9 +13,11 @@ end
 function widget:Initialize()
 	Spring.SendCommands("bind y settarget")
 	Spring.SendCommands("bind j canceltarget")
+	Spring.SendCommands("bind o jump")
 end
 
 function widget:Shutdown()
 	Spring.SendCommands("unbind y settarget")
 	Spring.SendCommands("unbind j canceltarget")
+	Spring.SendCommands("unbind o jump")
 end
