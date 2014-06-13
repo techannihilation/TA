ad6b7b5f5f82dc7dfb41b62858d4b1b7a66c9e7c
abma <spring@abma.de>
2014-06-13 20:39:20 +0200

    fix version for rapid usage
    springrts.com/wiki/Rapid

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
   name='Tech Annihilation',
   description='The New Generation Of War',
   shortname='TA',
-  version='v2.23-dev',
+  version='$VERSION',
   mutator='',
   game='',
   shortGame='',
