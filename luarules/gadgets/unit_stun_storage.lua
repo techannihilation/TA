a27a7c75cf3c4f73735959ef0acf34298da80168
nixtux <davehomer@googlemail.com>
2014-06-15 14:43:29 +0100

    Add some missing e stores for stun gadget

--- luarules/gadgets/unit_stun_storage.lua
+++ luarules/gadgets/unit_stun_storage.lua
@@ -4,9 +4,9 @@
 function gadget:GetInfo()
   return {
     name      = "Stun Storage",
-    desc      = "Makes stunned storage leak energy",
+    desc      = "Makes stunned storage leak/use energy",
     author    = "Nixtux",
-    date      = "April 29, 20012",
+    date      = "June 15, 2014",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
     enabled   = true  --  loaded by default?
@@ -28,10 +28,13 @@ local storageDefs = {
   [ UnitDefNames['armestor'].id ] = true,
   [ UnitDefNames['armuwadves'].id ] = true,
   [ UnitDefNames['armses'].id ] = true,
+  [ UnitDefNames['armuwes'].id ] = true,
+
   --Core 
   [ UnitDefNames['corestor'].id ] = true,
   [ UnitDefNames['coruwadves'].id ] = true,
   [ UnitDefNames['corses'].id ] = true,
+  [ UnitDefNames['coruwes'].id ] = true,
   --The Lost Legacy
   [ UnitDefNames['tllestor'].id ] = true,
   [ UnitDefNames['tllemstor'].id ] = true,
@@ -59,17 +62,19 @@ local pairs = pairs
 function gadget:GameFrame(n)
   if (((n+18) % 32) < 0.1) then
       for unitID, scriptIDs in pairs(storageunits) do
-		 
 	  local uDefID = GetUnitDefID(unitID) ; if not uDefID then break end
 	  local uDef = uDefs[uDefID]
 	  local storagecap = uDef.energyStorage
 	  local penatly = storagecap / 2500 
 	  
 	  if Spring.GetUnitIsStunned(unitID) then
-		Spring.Echo(unitID .. " is stunned  " ..storagecap,penatly)
+		--Spring.Echo(unitID .. " is stunned  " ..storagecap,penatly)
 		Spring.UseTeamResource(Spring.GetUnitTeam(unitID), "energy", penatly)
-	  end
+		     storageunits[unitID]= nil
+
+		  end
    end
+
   end
 end
 -------------------------------------------------------------------------------------
@@ -105,7 +110,7 @@ end
 
 function gadget:UnitDestroyed(unitID, unitDefID, unitTeam)
 	if (storageDefs[unitDefID]) then 
-		
+	   storageDefs[unitID]= nil
 	end
 end
 
