66513ff31c7b58594a1955e9b9c9d694c3979d24
nixtux <davehomer@googlemail.com>
2014-10-26 06:10:52 +0000

    Add new tll rez sub

---
+++ units/tllsr.lua
@@ -0,0 +1,92 @@
+-- UNITDEF -- TLLSR --
+--------------------------------------------------------------------------------
+
+local unitName = "tllsr"
+
+--------------------------------------------------------------------------------
+
+local unitDef = {
+	acceleration = 0.048,
+	bmcode = 1,
+	brakeRate = 0.25,
+	buildCostEnergy = 3234,
+	buildCostMetal = 563,
+	buildDistance = 112,
+	builder = true,
+	buildPic = [[TLLSR.png]],
+	buildTime = 9259,
+	canAssist = false,
+	canGuard = true,
+	canMove = true,
+	canPatrol = true,
+	canreclamate = 1,
+	canResurrect = true,
+	canstop = 1,
+	category = [[ALL MEDIUM MOBILE NOTDEFENSE NOTHOVERNOTVTOL NOTVTOL NOTWEAPON SUB]],
+	collisionvolumeoffsets = [[0 0 2]],
+	collisionVolumeScales = [[38 17 50]],
+	collisionvolumetype = [[box]],
+	defaultmissiontype = [[Standby]],
+	description = [[Ressurection Sub]],
+	energyMake = 2,
+	energyStorage = 0,
+	energyUse = 2,
+	explodeAs = [[SMALL_UNITEX]],
+	footprintX = 3,
+	footprintZ = 3,
+	iconType = [[sea]],
+	idleAutoHeal = 5,
+	idleTime = 1800,
+	maneuverleashlength = 640,
+	maxDamage = 670,
+	maxVelocity = 2.36,
+	metalStorage = 0,
+	minWaterDepth = 20,
+	mobilestandorders = 1,
+	movementClass = [[UBOAT3]],
+	name = [[SoulRaiZer]],
+	noAutoFire = false,
+	noChaseCategory = [[NOTSUBNOTSHIP]],
+	objectName = [[TLLSR]],
+	resurrect = 1,
+	seismicSignature = 0,
+	selfDestructAs = [[SMALL_UNIT]],
+	side = [[ARM]],
+	sightDistance = 156,
+	standingmoveorder = 1,
+	steeringmode = 1,
+	turnRate = 282,
+	unitname = [[tllsr]],
+	waterline = 30,
+	workerTime = 450,
+	sounds = {
+		build = [[nanlath1]],
+		canceldestruct = [[cancel2]],
+		capture = [[capture1]],
+		repair = [[repair1]],
+		underattack = [[warning1]],
+		working = [[reclaim1]],
+		cant = {
+			[1] = [[cantdo4]],
+		},
+		count = {
+			[1] = [[count6]],
+			[2] = [[count5]],
+			[3] = [[count4]],
+			[4] = [[count3]],
+			[5] = [[count2]],
+			[6] = [[count1]],
+		},
+		ok = {
+			[1] = [[sucormov]],
+		},
+		select = {
+			[1] = [[sucorsel]],
+		},
+	},
+}
+--------------------------------------------------------------------------------
+
+return lowerkeys({[unitName] = unitDef})
+
+--------------------------------------------------------------------------------
