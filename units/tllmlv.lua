7dcd0e5937fdddd6e29aa2ed1e4520f9a3e66e1d
nixtux <davehomer@googlemail.com>
2014-11-26 19:41:00 +0000

    Add tll minelayer and make it buildable

---
+++ units/tllmlv.lua
@@ -0,0 +1,199 @@
+-- UNITDEF -- TLLMLV --
+--------------------------------------------------------------------------------
+
+local unitName = "tllmlv"
+
+--------------------------------------------------------------------------------
+
+local unitDef = {
+	acceleration = 0.071,
+	activateWhenBuilt = true,
+	bmcode = 1,
+	brakeRate = 0.55,
+	buildCostEnergy = 900,
+	buildCostMetal = 193,
+	buildDistance = 28,
+	builder = true,
+	buildPic = [[TLLMLV.png]],
+	buildTime = 3921,
+	canAssist = false,
+	canGuard = false,
+	canMove = true,
+	canPatrol = false,
+	canReclaim = false,
+	canreclamate = 0,
+	canRepair = false,
+	canRestore = false,
+	canstop = 1,
+	category = [[ALL CONSTR MOBILE NOTDEFENSE NOTHOVERNOTVTOL NOTSUB NOTSUBNOTSHIP NOTVTOL TINY WEAPON]],
+	corpse = [[DEAD]],
+	defaultmissiontype = [[Standby]],
+	description = [[Stealthy Minelayer/Minesweeper]],
+	energyMake = 1,
+	energyStorage = 0,
+	energyUse = 1,
+	explodeAs = [[BIG_UNITEX]],
+	firestandorders = 1,
+	footprintX = 2,
+	footprintZ = 2,
+	idleAutoHeal = 5,
+	idleTime = 1800,
+	leaveTracks = true,
+	maneuverleashlength = 640,
+	mass = 1500,
+	maxDamage = 245,
+	maxSlope = 16,
+	maxVelocity = 2.524,
+	maxWaterDepth = 0,
+	metalMake = 0,
+	metalStorage = 0,
+	mobilestandorders = 1,
+	movementClass = [[TANK3]],
+	name = [[Podger]],
+	noChaseCategory = [[ALL SUB]],
+	objectName = [[TLLMLV]],
+	onoffable = false,
+	seismicSignature = 0,
+	selfDestructAs = [[BIG_UNIT]],
+	side = [[arm]],
+	sightDistance = 201,
+	standingfireorder = 0,
+	standingmoveorder = 1,
+	stealth = true,
+	steeringmode = 1,
+	terraformSpeed = 120,
+	trackOffset = 12,
+	trackStrength = 5,
+	trackStretch = 1,
+	trackType = [[StdTank]],
+	trackWidth = 30,
+	turnRate = 629,
+	unitname = [[tllmlv]],
+	workerTime = 40,
+	buildoptions = {
+		[1] = [[tlldt]],
+		[2] = [[tlltower]],
+		[3] = [[tllmine1]],
+		[4] = [[tllmine2]],
+		[5] = [[tllmine3]],
+		[6] = [[tllmine4]],
+		[7] = [[tllmine5]],
+		[8] = [[tllmine6]],
+	},
+	featureDefs = nil,
+	sounds = {
+		build = [[nanlath1]],
+		canceldestruct = [[cancel2]],
+		repair = [[repair1]],
+		underattack = [[warning1]],
+		working = [[reclaim1]],
+		cant = {
+			[1] = [[cantdo4]],
+		},
+		count = {
+			[1] = [[count6]],
+			[2] = [[count5]],
+			[3] = [[count4]],
+			[4] = [[count3]],
+			[5] = [[count2]],
+			[6] = [[count1]],
+		},
+		ok = {
+			[1] = [[varmmove]],
+		},
+		select = {
+			[1] = [[varmsel]],
+		},
+	},
+	weaponDefs = nil,
+	weapons = {
+		[1] = {
+			def = [[MINESWEEP]],
+			onlyTargetCategory = [[MINE]],
+		},
+	},
+}
+
+--------------------------------------------------------------------------------
+
+local weaponDefs = {
+	MINESWEEP = {
+		areaOfEffect = 512,
+		collideFriendly = false,
+		craterBoost = 0,
+		craterMult = 0,
+		edgeEffectiveness = 0.25,
+		explosionGenerator = [[custom:MINESWEEP]],
+		intensity = 0,
+		lineOfSight = false,
+		metalpershot = 0,
+		name = [[MineSweep]],
+		noSelfDamage = true,
+		range = 200,
+		reloadtime = 3,
+		renderType = 4,
+		rgbColor = [[0 0 0]],
+		thickness = 0,
+		tolerance = 100,
+		turret = true,
+		weaponTimer = 0.1,
+		weaponType = [[Cannon]],
+		weaponVelocity = 3650,
+		damage = {
+			default = 0,
+			subs = 5,
+		},
+	},
+}
+unitDef.weaponDefs = weaponDefs
+
+
+--------------------------------------------------------------------------------
+
+local featureDefs = {
+	DEAD = {
+		blocking = true,
+		category = [[corpses]],
+		collisionvolumeoffsets = [[0.399993896484 1.98730468792e-06 0.533332824707]],
+		collisionvolumescales = [[20.5333251953 11.8133239746 27.7333221436]],
+		collisionvolumetype = [[Box]],
+		damage = 0.6000 * unitDef.maxDamage,
+		description = unitDef.name .. [[ Wreckage]],
+		energy = 0,
+		featureDead = [[HEAP]],
+		featurereclamate = [[SMUDGE01]],
+		footprintX = 3,
+		footprintZ = 3,
+		height = 20,
+		hitdensity = 100,
+		metal = 0.8000 * unitDef.buildCostMetal,
+		object = [[TLLMLV_DEAD]],
+		reclaimable = true,
+		seqnamereclamate = [[TREE1RECLAMATE]],
+		world = [[All Worlds]],
+	},
+	HEAP = {
+		blocking = false,
+		category = [[heaps]],
+		damage = 0.3600 * unitDef.maxDamage,
+		description = unitDef.name .. [[ Heap]],
+		energy = 0,
+		featurereclamate = [[SMUDGE01]],
+		footprintX = 3,
+		footprintZ = 3,
+		height = 4,
+		hitdensity = 100,
+		metal = 0.6400 * unitDef.buildCostMetal,
+		object = [[3X3B]],
+		reclaimable = true,
+		seqnamereclamate = [[TREE1RECLAMATE]],
+		world = [[All Worlds]],
+	},
+}
+unitDef.featureDefs = featureDefs
+
+--------------------------------------------------------------------------------
+
+return lowerkeys({[unitName] = unitDef})
+
+--------------------------------------------------------------------------------
