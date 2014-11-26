5c73788ab4647cc1f29372de393ed6603d9f1db9
nixtux <davehomer@googlemail.com>
2014-11-26 19:40:17 +0000

    Add tll small emp mine

---
+++ weapons/twilight_small.lua
@@ -0,0 +1,39 @@
+-- WEAPONDEF -- TWILIGHT_SMALL --
+--------------------------------------------------------------------------------
+
+local weaponName = "twilight_small"
+
+--------------------------------------------------------------------------------
+
+local weaponDef = {
+	areaofeffect = 150,
+	ballistic = 1,
+	commandfire = 1,
+	craterboost = 0,
+	cratermult = 0,
+	edgeeffectiveness = 0.75,
+	explosiongenerator = [[custom:EMPFLASH240]],
+	firestarter = 90,
+	impulseboost = 0,
+	impulsefactor = 0,
+	model = [[bomb]],
+	name = [[EMPboom]],
+	paralyzer = 1,
+	paralyzetime = 10,
+	range = 600,
+	reloadtime = 0.30000001192093,
+	rendertype = 4,
+	soundhit = [[EMGPULS1]],
+	soundstart = [[bombrel]],
+	tolerance = 7000,
+	turret = 1,
+	damage = {
+		commanders = 30,
+		default = 4000,
+	},
+}
+--------------------------------------------------------------------------------
+
+return lowerkeys({[weaponName] = weaponDef})
+
+--------------------------------------------------------------------------------
