644d741f6ecff11bfbaf97bd167a9d81008648b2
nixtux <davehomer@googlemail.com>
2014-11-26 21:23:33 +0000

    Add thermite mine to tll

---
+++ weapons/thermite_mine.lua
@@ -0,0 +1,38 @@
+-- WEAPONDEF -- THERMITE_MINE --
+--------------------------------------------------------------------------------
+
+local weaponName = "thermite_mine"
+
+--------------------------------------------------------------------------------
+
+local weaponDef = {
+	areaofeffect = 64,
+	ballistic = 1,
+	commandfire = 1,
+	craterboost = 0,
+	cratermult = 0,
+	edgeeffectiveness = 0.75,
+	explosionGenerator = [[custom:thermite_mine]],
+	firestarter = 90,
+	impulseboost = 0,
+	impulsefactor = 0,
+	model = [[bomb]],
+	name = [[Thermite Blast]],
+	range = 600,
+	reloadtime = 0.30000001192093,
+	rendertype = 4,
+	soundHitDry = [[xplosml3]],
+	soundStart = [[canlite3]],
+	tolerance = 7000,
+	turret = 1,
+	damage = {
+		default = 1500,
+		minelayers = 25,
+		mines = 0.5,
+	},
+}
+--------------------------------------------------------------------------------
+
+return lowerkeys({[weaponName] = weaponDef})
+
+--------------------------------------------------------------------------------
