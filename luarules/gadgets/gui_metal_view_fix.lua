fbcbe6482bf2777642176c48e35b05b36d0ba360
nixtux <davehomer@googlemail.com>
2014-11-27 22:42:32 +0000

    Disable f4 before game start

--- luarules/gadgets/gui_metal_view_fix.lua
+++ luarules/gadgets/gui_metal_view_fix.lua
@@ -19,14 +19,17 @@ if (gadgetHandler:IsSyncedCode()) then --SYNCED
 local StartMViewtogged = nil
 
 function gadget:Initialize()
-  Spring.SendCommands ("toggleinfo metal")
+  Spring.SendCommands("unbind any+f4 showmetalmap")
+  Spring.SendCommands("toggleinfo metal")
   StartMViewtogged = true
 end
 
 function gadget:GameFrame(frame)
   if StartMViewtogged == true and frame > 1 then
-    Spring.SendCommands ("toggleinfo metal")
+    Spring.SendCommands("toggleinfo metal")
+    Spring.SendCommands("bind any+f4 showmetalmap")
     StartMViewtogged = false
+    gadgetHandler:RemoveGadget()
   end
 end
 
