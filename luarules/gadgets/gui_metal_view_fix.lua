74a78092e9091353052fc0d2775b9abd3fb351b0
nixtux <davehomer@googlemail.com>
2014-11-27 14:38:02 +0000

    Add gadget to enable metal view before gamestart -- spring 99 and greater only

---
+++ luarules/gadgets/gui_metal_view_fix.lua
@@ -0,0 +1,35 @@
+function gadget:GetInfo()
+  return {
+    name      = "Pre Start Metal View",
+    desc      = "Enables Mex view before game start",
+    author    = "nixtux",
+    date      = "Nov 27, 2014",
+    license   = "GNU GPL, v2 or later",
+    layer     = 0,
+    enabled   = true  --  loaded by default?
+  }
+end
+
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+if (gadgetHandler:IsSyncedCode()) then --SYNCED
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+
+local StartMViewtogged = nil
+
+function gadget:Initialize()
+  Spring.SendCommands ("toggleinfo metal")
+  StartMViewtogged = true
+end
+
+function gadget:GameFrame(frame)
+  if StartMViewtogged == true and frame > 1 then
+    Spring.SendCommands ("toggleinfo metal")
+    StartMViewtogged = false
+  end
+end
+
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+end
