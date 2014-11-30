ebea3e451897ee56558d6641b0aa4b6d8b1c60a1
nixtux <davehomer@googlemail.com>
2014-11-30 09:10:11 +0000

    Update radar overlay widget, Dont toggle los view if already in los view ,also enables los voew on mouseover radar towers

--- luaui/widgets/gui_radar_overlay.lua
+++ luaui/widgets/gui_radar_overlay.lua
@@ -2,7 +2,7 @@
 function widget:GetInfo()
 	return {
 		name      = "Radar Overlay",
-		desc      = "Enabled Overlay mode while doing radar placement",
+		desc      = "Enabled Los View Overlay mode when placing radat towers or mouse over radar tower",
 		author    = "Nix",
 		version   = "v0.1",
 		date      = "Nov 2014",
@@ -17,31 +17,59 @@ end
 -- Speedups
 ------------------------------------------------------------
 local spGetActiveCommand = Spring.GetActiveCommand
+local spGetMapDrawMode = Spring.GetMapDrawMode
 
 local isRadar = {}
+
 for uDefID, uDef in pairs(UnitDefs) do
-	if uDef.radarRadius > 600 then
-	  Spring.Echo("radar")
-		isRadar[uDefID] = true
-	end
+  if uDef.radarRadius > 1300 then --Should only seletect real radar units
+    isRadar[uDefID] = true
+  end
 end
 
 local Overlayenabled = false
-
+local MouseOver = false
 ------------------------------------------------------------
 -- Callins
 ------------------------------------------------------------
 
 
 function widget:GameFrame(frame)
-  
-  if frame < 1 then return end
   if ((frame%8)<1) then
-    -- Check command is to build a mex
+    
+    if spGetMapDrawMode() == 'los' and Overlayenabled == false and MouseOver == false then --Disable if player play's with los view on
+      return
+    end
+    
+    --Enable los view for mouse over
+    local x, y = Spring.GetMouseState()
+    local isunit, unitID = Spring.TraceScreenRay(x, y)
+    if (isunit == "unit") then
+      local unitDefID = Spring.GetUnitDefID(unitID)
+      if isRadar[unitDefID] then
+        if MouseOver == false then
+          Spring.SendCommands ("toggleinfo los")
+          MouseOver = true
+	end
+      else
+	if MouseOver == true then
+	  Spring.SendCommands ("toggleinfo los")
+          MouseOver = false
+        end
+      end
+    else
+	if MouseOver == true then
+	  Spring.SendCommands ("toggleinfo los")
+          MouseOver = false
+        end
+	
+    end
+    
+    --Enable los viwe while placing radar towers
     local _, cmdID = spGetActiveCommand()
     if (cmdID and isRadar[-cmdID]) then
       if Overlayenabled == false then
-        Spring.SendCommands ("toggleinfo los") --todo how to work out what mode we are using
+        Spring.SendCommands ("toggleinfo los")
         Overlayenabled = true
       end
     else 
