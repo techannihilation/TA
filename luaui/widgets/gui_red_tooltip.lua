51a47580df8f4cdb26d933e41a60fd237a956e91
nixtux <davehomer@googlemail.com>
2015-04-09 18:23:25 +0100

    Fix incorrect xp values showing

--- luaui/widgets/gui_red_tooltip.lua
+++ luaui/widgets/gui_red_tooltip.lua
@@ -120,7 +120,7 @@ local function getEditedCurrentTooltip()
 	local currentExp = tonumber(text:match(expPattern))
 	local limExp = currentExp and currentExp/(1+currentExp) or 1
 	--replace with limexp: exp/(1+exp) since all spring exp effects are linear in limexp, multiply by 10 because people like big numbers instead of [0,1]
-	text = currentExp and text:gsub(expPattern,string.format("Experience %.2f", 10*limExp) ) or text
+	text = currentExp and text:gsub(expPattern,string.format("Experience %.2f", currentExp) ) or text
 	if text:find("Maverick") then --special hack to show mav updated maxrange
 		local rangePattern = "Range (%d+)"
 		local currentRange = tonumber(text:match(rangePattern))
