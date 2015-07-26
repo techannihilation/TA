295ef7a943be8f41deaf9bbbaeec1c756b77153e
nixtux <davehomer@googlemail.com>
2015-07-26 11:14:20 +0100

    Test commit

--- luaintro/Addons/bg_texture.lua
+++ luaintro/Addons/bg_texture.lua
@@ -15,8 +15,7 @@ end
 ------------------------------------------
 
 local loadscreens = VFS.DirList("bitmaps/loadpictures/")
-local screenNum = math.random(#loadscreens) 
---local backgroundTexture = loadscreens[screenNum]
+local screenNum = math.random(#loadscreens)
 local backgroundTexture = loadscreens[1+(math.floor((1000*os.clock())%#loadscreens))] -- hacky hotfix for http://springrts.com/mantis/view.php?id=4572
 local aspectRatio
 
