ef25cf0e801e6499c558c4b5f40424eafebfddbb
nixtux <davehomer@googlemail.com>
2015-08-02 21:28:52 +0100

    Cleanup/Whitespace/Comment

--- luarules/gadgets/unit_stun_metal_storage.lua
+++ luarules/gadgets/unit_stun_metal_storage.lua
@@ -58,38 +58,36 @@ local pairs = pairs
 
 function gadget:GameFrame(n)
   if (((n+18) % 32) < 0.1) then
-      for unitID, _ in pairs(storageunits) do
-	  local uDefID = GetUnitDefID(unitID) ; if not uDefID then break end
-	  local uDef = uDefs[uDefID]
-	  local storage = storageunits[unitID].storage
-	  local _,stunned = Spring.GetUnitIsStunned(unitID)
-	  
-	  if stunned and (storageunits[unitID].paracount == 0) then
-		local currentLevel,totalstorage = Spring.GetTeamResources(Spring.GetUnitTeam(unitID),"metal")
-		local newstoragetotal = totalstorage - storage
-		--Spring.Echo("current storage level " .. currentLevel .. "   total storage " .. totalstorage .. "   new storage level " ..newstoragetotal)
-
-		Spring.SetTeamResource(Spring.GetUnitTeam(unitID), "ms", newstoragetotal)
-		      if currentLevel > (newstoragetotal) then
-				local x,y,z = Spring.GetUnitPosition(unitID)
-				local height = storageunits[unitID].height * 0.70
-				Spring.SpawnCEG("METAL_STORAGE_LEAK",x,y+height,z,0,0,0)
-		      end
-		      storageunits[unitID].paracount = 1
-		      stunnedstorage[unitID] = true 
-	  end
-    end
-  for unitID,_ in pairs(stunnedstorage) do
-	  
-	    local _,stunned = Spring.GetUnitIsStunned(unitID)
+    for unitID, _ in pairs(storageunits) do
+	    local uDefID = GetUnitDefID(unitID) ; if not uDefID then break end
+	    local uDef = uDefs[uDefID]
 	    local storage = storageunits[unitID].storage
-	    if not stunned then
-	    local _,totalstorage = Spring.GetTeamResources(Spring.GetUnitTeam(unitID),"metal")
-	    Spring.SetTeamResource(Spring.GetUnitTeam(unitID), "ms", totalstorage+storage)
-	    stunnedstorage[unitID] = nil
-	    storageunits[unitID].paracount = 0
-	    end
-	  end
+	    local _,stunned = Spring.GetUnitIsStunned(unitID)
+	  
+	    if stunned and (storageunits[unitID].isEMPed == 0) then
+		    local currentLevel,totalstorage = Spring.GetTeamResources(Spring.GetUnitTeam(unitID),"metal")
+		    local newstoragetotal = totalstorage - storage
+		    --Spring.Echo("current storage level " .. currentLevel .. "   total storage " .. totalstorage .. "   new storage level " ..newstoragetotal)
+  		  Spring.SetTeamResource(Spring.GetUnitTeam(unitID), "ms", newstoragetotal)
+		    if currentLevel > (newstoragetotal) then
+				  local x,y,z = Spring.GetUnitPosition(unitID)
+				  local height = storageunits[unitID].height * 0.70
+				  Spring.SpawnCEG("METAL_STORAGE_LEAK",x,y+height,z,0,0,0)
+		    end
+		    storageunits[unitID].isEMPed = 1
+		    stunnedstorage[unitID] = true 
+	     end
+    end
+    for unitID,_ in pairs(stunnedstorage) do
+	     local _,stunned = Spring.GetUnitIsStunned(unitID)
+	     local storage = storageunits[unitID].storage
+	     if not stunned then
+	       local _,totalstorage = Spring.GetTeamResources(Spring.GetUnitTeam(unitID),"metal")
+	       Spring.SetTeamResource(Spring.GetUnitTeam(unitID), "ms", totalstorage+storage)
+	       stunnedstorage[unitID] = nil
+	       storageunits[unitID].isEMPed = 0
+	     end
+	   end
   end
 end
 -------------------------------------------------------------------------------------
@@ -98,7 +96,7 @@ end
 local function SetupUnit(unitID,unitDefID)
   local ud = UnitDefs[unitDefID]
   if (ud == nil)or(ud.height == nil) then return nil end
-   storageunits[unitID] = {paracount = 0, height = ud.height, storage = ud.metalStorage}
+   storageunits[unitID] = {isEMPed = 0, height = ud.height, storage = ud.metalStorage}
 end
 
 --testing only
@@ -128,15 +126,15 @@ end
 function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponID, projectileID, attackerID, attackerDefID, attackerTeam) --we use UnitPreDamaged so as we get in before unit_transportfix has its effect
   if (storageDefs[unitDefID]) then
     local hp = Spring.GetUnitHealth(unitID)
-    if damage > hp then  --seems unit can have 0 health
-      if storageunits[unitID] and storageunits[unitID].paracount == 1 then
+    if damage > hp then  --unit can have 0 health
+      if storageunits[unitID] and storageunits[unitID].isEMPed == 1 then
         local _,totalstorage = Spring.GetTeamResources(Spring.GetUnitTeam(unitID),"metal")
-          Spring.SetTeamResource(Spring.GetUnitTeam(unitID), "ms", totalstorage + storageunits[unitID].storage)
-        end
-        storageunits[unitID]= nil
-        stunnedstorage[unitID] = nil
+        Spring.SetTeamResource(Spring.GetUnitTeam(unitID), "ms", totalstorage + storageunits[unitID].storage) --Add back before unit is destoryed
       end
+    storageunits[unitID]= nil
+    stunnedstorage[unitID] = nil
     end
+  end
 end
 
 -------------------------------------------------------------------------------------
