a9d3558a4f0935f346ce65b6df9a637a39946157
skymyj <skymyj@hotmail.com>
2015-09-14 13:46:25 +0200

    Update tllcom6.lua

--- units/tllcom6.lua
+++ units/tllcom6.lua
@@ -67,7 +67,7 @@ return {
 		script = "tllcom.cob",
 		seismicsignature = 0,
 		selfdestructas = "COMMANDER_SELFD",
-		selfdestructcountdown = 20,
+		selfdestructcountdown = 10,
 		showplayername = true,
 		side = "TLL",
 		sightdistance = 850,
