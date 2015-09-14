3876a8a42bb8f8b55ac22443b3b59a25f6299ff3
skymyj <skymyj@hotmail.com>
2015-09-14 13:44:00 +0200

    Update corcom3.lua

--- units/corcom3.lua
+++ units/corcom3.lua
@@ -66,7 +66,7 @@ return {
 		script = "corcom.cob",
 		seismicsignature = 0,
 		selfdestructas = "COMMANDER_SELFD",
-		selfdestructcountdown = 20,
+		selfdestructcountdown = 10,
 		showplayername = true,
 		side = "CORE",
 		sightdistance = 600,
