bfe473d76c260d7697169230fda0c09ea4ae8171
nixtux <davehomer@googlemail.com>
2016-03-20 09:50:02 +0000

    Disable tidal rock gadget, update tll sea units scripts to match

--- luarules/gadgets/unit_tidal_rock_control.lua
+++ luarules/gadgets/unit_tidal_rock_control.lua
@@ -9,7 +9,7 @@ function gadget:GetInfo()
     date      = "Aug 13, 2013",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = true  --  loaded by default?
+    enabled   = false    --  loaded by default?
   }
 end
 
