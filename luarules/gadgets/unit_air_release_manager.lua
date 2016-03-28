df4f7a074ebe9a7535a91bc69f4ae513b8d066c0
nixtux <davehomer@googlemail.com>
2016-03-28 13:13:52 +0100

    Update for spring 102

--- luarules/gadgets/unit_air_release_manager.lua
+++ luarules/gadgets/unit_air_release_manager.lua
@@ -1,12 +1,12 @@
 function gadget:GetInfo()
   return {
     name      = "Air Release",
-    desc      = "Makes t1 air drops a bit smoother",
-    author    = "TheFatController",
+    desc      = "Adds falling damage = falling height * mass / 50",
+    author    = "Pako",
     date      = "21 Feb 2010",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = false  --  loaded by default?
+    enabled   = true  --  loaded by default?
   }
 end
 
@@ -14,35 +14,50 @@ if (not gadgetHandler:IsSyncedCode()) then
   return
 end
 
-local CAN_RELEASE = {
-  [UnitDefNames["corvalk"].id] = 25,
-  [UnitDefNames["armatlas"].id] = 25,
-  [UnitDefNames["tlltplane"].id] = 25,
-  [UnitDefNames["armdragf"].id] = 25,
-  [UnitDefNames["corfalc"].id] = 25,
-}
-
 local COMMANDO = UnitDefNames["commando"].id
 
-function gadget:UnitDestroyed(unitID, unitDefID, unitTeam, attackerID)
-	if CAN_RELEASE[unitDefID] then
-		local unitList = Spring.GetUnitIsTransporting(unitID)
-		if unitList[1] then
-			for _,transUnitID in ipairs(unitList) do
-			    local transUnitDefID = Spring.GetUnitDefID(transUnitID)
-			    if transUnitDefID ~= COMMANDO then
-					local x,y,z = Spring.GetUnitBasePosition(transUnitID)
-					local h = Spring.GetGroundHeight(x,z)
-					if (y-h) > CAN_RELEASE[unitDefID] then
-						Spring.AddUnitDamage(transUnitID, 10000000, 0, -1)
-					else
-						Spring.AddUnitDamage(transUnitID, ((y-h)*25), 0, attackerID or -1)
-					end
-				else
-				  local x,_,z = Spring.GetUnitVelocity(transUnitID)
-				  Spring.AddUnitImpulse(transUnitID,x*0.5,4,z*0.5)
-				end
-			end
-		end	
-	end
+local fC = 0
+local fallingUnits = {}
+
+function gadget:GameFrame(n)
+  if fC > 0 then
+    for k,t in ipairs(fallingUnits) do
+        local unitAlive = (Spring.ValidUnitID (t[1]) and (not Spring.GetUnitIsDead (t[1]))) --+++
+        if (unitAlive) then                        --++
+            x,y,z = Spring.GetUnitBasePosition(t[1])        --maybe check if units were off map        
+            h = Spring.GetGroundHeight(x,z)
+        --else
+            --Spring.Echo ("unit was not alive anymore. There would have been error spam")
+        end
+        if (not unitAlive or h > y-10)  then            --+++
+          Spring.AddUnitDamage(t[1], t[2])
+          table.remove(fallingUnits,k)
+          fC = fC - 1
+        end
+    end
+  end
+end
+
+function gadget:UnitUnloaded(unitID, unitDefID, _, transID)
+    if not transID then
+        return 
+    end
+    
+    local transDefID = Spring.GetUnitDefID(transID)
+    if not transDefID or UnitDefs[transDefID].isAirBase then 
+        return 
+    end
+    
+    local x,y,z = Spring.GetUnitBasePosition(unitID)
+    local h = math.max(0,Spring.GetGroundHeight(x,z))
+    local damage = 0
+    if (y-h) > 25 then
+        local Udef = UnitDefs[unitDefID]
+        damage = Udef.mass * (y-h)/50
+    end
+    if damage > 10 then 
+        fC = fC + 1
+        --Spring.Echo(y-h, damage)
+        fallingUnits[fC] = {unitID, damage}
+    end
 end
