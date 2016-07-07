0e738e4534208c0fd0256b24d759c643c71e87d9
nixtux <davehomer@googlemail.com>
2016-07-07 17:10:27 +0100

    Remove hardcoded warning message

--- luaintro/addons/main.lua
+++ luaintro/addons/main.lua
@@ -17,7 +17,7 @@ end
 local lastLoadMessage = ""
 local infoMessage = {}
 
-table.insert(infoMessage, "Spring Engine version 101.0.1-201 or above is needed for this version")
+--table.insert(infoMessage, "Spring Engine version 101.0.1-201 or above is needed for this version")
 
 if Spring.GetModOptions() then 
 	if Spring.GetModOptions().deathmode then
