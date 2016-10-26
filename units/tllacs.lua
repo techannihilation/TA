0c9b93f96806bde4ffce011f05f4262f57092dfa
skymyj <skymyj@hotmail.com>
2016-10-27 00:00:24 +0200

    Update tllacs.lua

--- units/tllacs.lua
+++ units/tllacs.lua
@@ -3,7 +3,7 @@ return {
 		acceleration = 0.11,
 		brakerate = 0.9,
 		buildcostenergy = 28480,
-		buildcostmetal = 3455,
+		buildcostmetal = 3255,
 		builddistance = 312,
 		builder = true,
 		buildtime = 15585,
