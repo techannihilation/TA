6b6fb640700f7742cf14bba49efd597f8d1a89af
skymyj <skymyj@hotmail.com>
2016-12-10 13:59:14 +0100

    Update tllmex.lua

--- units/tllmex.lua
+++ units/tllmex.lua
@@ -9,6 +9,7 @@ return {
 		buildinggrounddecalsizex = 5,
 		buildinggrounddecalsizey = 5,
 		buildinggrounddecaltype = "tllmex_aoplane.dds",
+		buildpic = "tllmex.png",
 		buildtime = 1820,
 		category = "ALL NOTDEFENSE NOTHOVERNOTVTOL NOTMOBILE NOTSUB NOTSUBNOTSHIP NOTVTOL NOTWEAPON",
 		collisionvolumeoffsets = "0 12 -7",
