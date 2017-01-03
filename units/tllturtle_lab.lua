1fd442272bc1b1b3cc5a1611fc374f7ecd107028
nixtux <davehomer@googlemail.com>
2017-01-03 13:41:33 +0000

    Fix missing category, Closes #24

--- units/tllturtle_lab.lua
+++ units/tllturtle_lab.lua
@@ -12,7 +12,7 @@ return {
 		canmove = true,
 		canpatrol = true,
 		canstop = 1,
-		category = "ALL NOTDEFENSE NOTHOVERNOTVTOL NOTMOBILE NOTSUB NOTSUBNOTSHIP NOTVTOL NOTWEAPON PLANT",
+		category = "ALL NOTDEFENSE NOTHOVERNOTVTOL NOTMOBILE NOTSUB NOTSUBNOTSHIP NOTVTOL NOTWEAPON PLANT LEVEL1",
 		corpse = "dead",
 		description = "Produces Kbots",
 		energystorage = 100,
