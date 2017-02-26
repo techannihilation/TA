1994d50d50c38f11c60424b70be48d04a49d22e6
nixtux <davehomer@googlemail.com>
2017-02-26 08:16:03 +0000

    Whitespace cleanup

--- luaui/widgets/gui_nano_range.lua
+++ luaui/widgets/gui_nano_range.lua
@@ -1,13 +1,13 @@
 function widget:GetInfo()
-  return {
-    name      = "Nano Range",
-    desc      = "Draws range for nanos if placing buildings/units",
-    author    = "Nixtux", --based on rings widget by Niobium
-    date      = "06/07/2014",
-    license   = "Public Domain",
-    layer     = 0,
-    enabled   = true  --  loaded by default?
-  }
+    return {
+        name      = "Nano Range",
+        desc      = "Draws range for nanos if placing buildings/units",
+        author    = "Nixtux", --based on rings widget by Niobium
+        date      = "06/07/2014",
+        license   = "Public Domain",
+        layer     = 0,
+        enabled   = true  --  loaded by default?
+     }
 end
 
 local pairs = pairs
@@ -39,7 +39,7 @@ function widget:Initialize()
     widgetHandler:RegisterGlobal('SetOpacity_Nanorings', SetOpacity) 
     for uDefId, ud in ipairs(UnitDefs) do
 	if ud.isBuilder and not ud.canMove and not ud.isFactory then
-		ringsDefs[uDefId] = { r = ud.nanoColorR, g = ud.nanoColorG, b = ud.nanoColorB, radius = ud.buildDistance }
+	    ringsDefs[uDefId] = { r = ud.nanoColorR, g = ud.nanoColorG, b = ud.nanoColorB, radius = ud.buildDistance }
 	end
     end
     for _, uId in pairs(SpGetAllUnits()) do
@@ -64,20 +64,19 @@ end
 function widget:DrawWorld()
     local _, cmdID = SpGetActiveCommand()
     if cmdID and cmdID < 0 then
-    for uId, rings in pairs(ringedUnits) do
-			local ux, uy, uz = SpGetUnitPosition(uId)
-			if ux then
-				  local r,g,b = rings.r,rings.g,rings.b
-				  --Spring.Echo(r,g,b,lightOpacity)
-					glColor(r,g,b,lightOpacity)
-					glLineWidth(1)
-					glDrawGroundCircle(ux, uy, uz, rings.radius, 24)
-			else
-				ringedUnits[uId] = nil
-			end
-		end
-	end
-    
+        for uId, rings in pairs(ringedUnits) do
+	local ux, uy, uz = SpGetUnitPosition(uId)
+	    if ux then
+	        local r,g,b = rings.r,rings.g,rings.b
+	        --Spring.Echo(r,g,b,lightOpacity)
+	        glColor(r,g,b,lightOpacity)
+                glLineWidth(1)
+	        glDrawGroundCircle(ux, uy, uz, rings.radius, 24)
+	    else
+	        ringedUnits[uId] = nil
+	    end
+         end
+    end
     glLineWidth(1)
     glColor(1, 1, 1, 1)
 end
@@ -86,3 +85,4 @@ function widget:GameOver()
     widgetHandler:DeregisterGlobal('SetOpacity_Nanorings', SetOpacity) 
     widgetHandler:RemoveWidget() 
 end 
+ 
