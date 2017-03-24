cad81637e41b70f4338dccc930044b57ff87568a
ksilversurfer <ksilversurfer@users.noreply.github.com>
2017-03-24 18:07:32 +0100

    Wrong faction

--- units/tllsr.lua
+++ units/tllsr.lua
@@ -47,7 +47,7 @@ return {
 		resurrect = 1,
 		seismicsignature = 0,
 		selfdestructas = "SMALL_UNIT",
-		side = "ARM",
+		side = "TLL",
 		sightdistance = 156,
 		standingmoveorder = 1,
 		steeringmode = 1,
