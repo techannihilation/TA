8e756d7e9530988b27d5d848ec039ecfc2a6f6d7
skymyj <skymyj@hotmail.com>
2017-04-04 23:36:51 +0200

    Update nsaatorph.lua

--- units/nsaatorph.lua
+++ units/nsaatorph.lua
@@ -48,7 +48,7 @@ return {
 		shootme = 1,
 		side = "ARM",
 		sightdistance = 280,
-		sonardistance = 305,
+		sonardistance = 500,
 		standingfireorder = 2,
 		standingmoveorder = 1,
 		steeringmode = 1,
