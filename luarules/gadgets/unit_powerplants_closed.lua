8ae0cabdae6e866b7ec36560b10e624c907960fe
nixtux <davehomer@googlemail.com>
2017-04-14 08:16:46 +0100

    Remove old neg energyusage for closed powerplants

---
+++ luarules/gadgets/unit_powerplants_closed.lua
@@ -0,0 +1,42 @@
+function gadget:GetInfo()
+  return {
+    name      = "Closed PowerPlants",
+    desc      = "Closed PowerPlantsar collectors don't produce energy",
+    author    = "Bluestone",
+    date      = "",
+    license   = "Hats",
+    layer     = 0,
+    enabled   = true  --  loaded by default?
+  }
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+if (not gadgetHandler:IsSyncedCode()) then
+  return
+end
+
+local noClosedE = {
+    [UnitDefNames.corsolar.id] = true,
+    [UnitDefNames.armsolar.id] = true,
+    [UnitDefNames.crnns.id] = true,
+    [UnitDefNames.tllsolar.id] = true,
+    [UnitDefNames.tllsolarns.id] = true,
+    [UnitDefNames.tlladvsolar.id] = true,
+}
+
+function gadget:UnitCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions)
+    if noClosedE[unitDefID] and cmdID==CMD.ONOFF then
+        local on = (cmdOptions[1]==1) --invert because the command hasn't yet taken effect
+        local _,_,eMake,_ = Spring.GetUnitResources(unitID)
+        if on then 
+            Spring.SetUnitResourcing(unitID, "ume", UnitDefs[unitDefID].energyMake - eMake)
+        else
+            Spring.SetUnitResourcing(unitID, "ume", -eMake)
+        end
+    end
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
