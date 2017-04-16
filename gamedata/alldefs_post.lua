608ba4deb5fe646cc50bfd51cf01afbc1fcbe599
nixtux <davehomer@googlemail.com>
2017-04-16 16:48:13 +0100

    fix alldefs mk2

--- gamedata/alldefs_post.lua
+++ gamedata/alldefs_post.lua
@@ -17,7 +17,7 @@
 -- Switch for when we want to save defs into customparams as strings (so as a widget can then write them to file)
 -- The widget to do so can be found in 'etc/Lua/bake_unitdefs_post'
 
-SaveDefsToCustomParams = true
+SaveDefsToCustomParams = false
 
 local iswater = {
 	["armfnanotc1"] = true,
