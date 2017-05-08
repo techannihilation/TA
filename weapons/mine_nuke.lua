62110f8603f55b6214ee6ef08d5e297b11a4bd57
skymyj <skymyj@hotmail.com>
2017-05-08 15:49:46 +0200

    Update mine_nuke.lua

--- weapons/mine_nuke.lua
+++ weapons/mine_nuke.lua
@@ -6,24 +6,24 @@ local weaponName = "mine_nuke"
 --------------------------------------------------------------------------------
 
 local weaponDef = {
-	areaofeffect = 768,
+	areaofeffect = 700,
 	craterboost = 5,
 	cratermult = 2.5,
-	edgeeffectiveness = 0.20000000298023,
+	edgeeffectiveness = 0.2,
 	explosiongenerator = [[custom:FLASHNUKE768]],
 	impulseboost = 0.25,
 	impulsefactor = 0.25,
 	name = [[NuclearMine]],
 	range = 1000,
-	reloadtime = 3.5999999046326,
+	reloadtime = 3.6,
 	soundhit = [[xplonuk3]],
 	soundstart = [[largegun]],
 	waterweapon = 1,
 	weaponvelocity = 250,
 	damage = {
 		commanders = 1500,
-		default = 3500,
-		mines = 20,
+		default = 6000,
+		mines = 50,
 	},
 }
 --------------------------------------------------------------------------------
