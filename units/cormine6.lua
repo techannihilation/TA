6a612f1250ea1db74af33fbb985086a36bedcbc8
skymyj <skymyj@hotmail.com>
2017-05-08 13:32:37 +0200

    Update cormine6.lua

--- units/cormine6.lua
+++ units/cormine6.lua
@@ -4,7 +4,7 @@ return {
 		activatewhenbuilt = false,
 		blocking = false,
 		brakerate = 0,
-		buildcostenergy = 16000,
+		buildcostenergy = 21000,
 		buildcostmetal = 375,
 		builder = false,
 		buildinggrounddecaldecayspeed = 30,
