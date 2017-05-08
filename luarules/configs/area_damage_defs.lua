bec10dfab9e84ae3c8b3b63176280558f276b56f
skymyj <skymyj@hotmail.com>
2017-05-08 16:02:46 +0200

    Update area_damage_defs.lua

--- luarules/configs/area_damage_defs.lua
+++ luarules/configs/area_damage_defs.lua
@@ -6,7 +6,7 @@ local weapons = {
 	tllacid_acidrain_rocket = { radius = 400, damage = 100, duration = 625, rangeFall = 0.6, timeFall = 0.5},
 	armsonic_sonic_cannon = { radius = 95, damage = 300, duration = 45, rangeFall = 0.25, timeFall = 0.8, allyScale = 0.5, teamScale = 0.25},
 	tllriot_tllriot_cannon = { radius = 75, damage = 200, duration = 35, rangeFall = 0.25, timeFall = 0.6, allyScale = 0.5 },
-	thermite_mine = { radius = 64, damage = 900, duration = 150, rangeFall = 0.5, timeFall = 0.5},
+	thermite_mine = { radius = 64, damage = 450, duration = 100, rangeFall = 0.5, timeFall = 0.5},
 	tllaether_thermite_bomb = { radius = 64, damage = 900, duration = 150, rangeFall = 0.5, timeFall = 0.5},
 
 	--junos
