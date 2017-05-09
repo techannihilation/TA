f135bec378b88a6813b291f4956e652ece5c769d
skymyj <skymyj@hotmail.com>
2017-05-09 13:28:31 +0200

    Balance Mines (#231)
    * Update cormine1.lua
    
    * Update cormine2.lua
    
    * Update cormine2.lua
    
    * Update cormine3.lua
    
    * Update cormine5.lua
    
    * Update cormine6.lua
    
    * Update cormine7.lua
    
    * Update cormine6.lua
    
    * Update cormine6.lua
    
    * Update cormine6.lua
    
    * Update cormine7.lua
    
    * Update armmine1.lua
    
    * Update armmine2.lua
    
    * Update armmine3.lua
    
    * Update armmine5.lua
    
    * Update armmine6.lua
    
    * Update armmine7.lua
    
    * Update tllmine1.lua
    
    * Update tllmine2.lua
    
    * Update tllmine3.lua
    
    * Update tllmine4.lua
    
    * Update tllmine5.lua
    
    * Update tllmine6.lua
    
    * Update mine_light.lua
    
    * Update mine_medium.lua
    
    * Update mine_heavy.lua
    
    * Update twilight.lua
    
    * Update mine_nuke.lua
    
    * Update mine_heavy.lua
    
    * Update mine_medium.lua
    
    * Update mine_medium.lua
    
    * Update mine_singularity.lua
    
    * Update mine_light.lua
    
    * Update mine_medium.lua
    
    * Update mine_medium.lua
    
    * Update mine_light.lua
    
    * Update mine_nuke.lua
    
    * Update mine_singularity.lua
    
    * Update mine_heavy.lua
    
    * Update thermite_mine.lua
    
    * Update tllmine4.lua
    
    * Update tllmine4.lua
    
    * Update thermite_mine.lua

--- weapons/mine_medium.lua
+++ weapons/mine_medium.lua
@@ -6,22 +6,22 @@ local weaponName = "mine_medium"
 --------------------------------------------------------------------------------
 
 local weaponDef = {
-	areaofeffect = 250,
+	areaofeffect = 150,
 	craterboost = 0,
 	cratermult = 0,
-	edgeeffectiveness = 0.60000002384186,
+	edgeeffectiveness = 0.6,
 	explosiongenerator = [[custom:FLASHMEDIUMBUILDING]],
 	impulseboost = 0,
 	name = [[MediumMine]],
 	range = 480,
-	reloadtime = 3.5999999046326,
+	reloadtime = 3,
 	soundhit = [[xplomed1]],
 	soundstart = [[largegun]],
 	weaponvelocity = 250,
 	damage = {
-		default = 1000,
+		default = 750,
 		minelayers = 20,
-		mines = 0.5,
+		mines = 1,
 	},
 }
 --------------------------------------------------------------------------------
