da407a58bece60ba024523506939109277143fb9
skymyj <skymyj@hotmail.com>
2017-05-18 13:44:40 +0200

    Update tllcom6.lua

--- units/tllcom6.lua
+++ units/tllcom6.lua
@@ -51,7 +51,7 @@ return {
 		maxvelocity = 1.5,
 		maxwaterdepth = 35,
 		metalmake = 12,
-		metalstorage = 5000,
+		metalstorage = 2500,
 		mincloakdistance = 40,
 		mobilestandorders = 1,
 		movementclass = "AKBOT2",
