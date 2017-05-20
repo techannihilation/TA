d04ea83b3eae2acb70b9c1cd89cd2bc529730030
skymyj <skymyj@hotmail.com>
2017-05-20 20:56:06 +0200

    Update tllmex1.lua

--- units/tllmex1.lua
+++ units/tllmex1.lua
@@ -20,7 +20,7 @@ return {
 		digger = 1,
 		energyuse = 22,
 		explodeas = "SMALL_BUILDINGEX",
-		extractsmetal = 0.0025,
+		extractsmetal = 0.0028,
 		footprintx = 3,
 		footprintz = 3,
 		icontype = "building",
