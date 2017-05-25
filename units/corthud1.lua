0435ae5117158795eb24bf4ab971a3791763a0c2
skymyj <skymyj@hotmail.com>
2017-05-25 19:59:22 +0200

    Update corthud1.lua

--- units/corthud1.lua
+++ units/corthud1.lua
@@ -136,7 +136,7 @@ return {
 				noselfdamage = true,
 				predictboost = 0.4,
 				range = 385,
-				reloadtime = 1,75,
+				reloadtime = 1.75,
 				rgbcolor = "1 0.86 0.11",
 				separation = 0.45,
 				size = 1.42,
