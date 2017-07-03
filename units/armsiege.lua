5641f27a74f6871f644a3abf50b50c639d9f6b52
skymyj <skymyj@hotmail.com>
2017-07-03 14:58:23 +0200

    Update armsiege.lua

--- units/armsiege.lua
+++ units/armsiege.lua
@@ -30,7 +30,7 @@ return {
 		losemitheight = 34,
 		maneuverleashlength = 640,
 		mass = 7500,
-		maxdamage = 29500,
+		maxdamage = 26500,
 		maxslope = 12,
 		maxvelocity = 1.2,
 		maxwaterdepth = 0,
