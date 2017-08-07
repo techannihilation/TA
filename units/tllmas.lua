0e0f3862db99572ac3345b7469dcddb6e7360912
ksilversurfer <ksilversurfer@users.noreply.github.com>
2017-08-07 12:45:35 +0200

    Name update

--- units/tllmas.lua
+++ units/tllmas.lua
@@ -17,7 +17,7 @@ return {
 		collisionvolumescales = "76 152 76",
 		collisionvolumetest = 1,
 		collisionvolumetype = "CylY",
-		description = "Planet Mantle Penetration Mine, Amphibious",
+		description = "Planet Mantle Penetration Mine (Metal Extractor), Amphibious",
 		energymake = 0,
 		energyuse = 250,
 		explodeas = "ATOMIC_BLAST",
