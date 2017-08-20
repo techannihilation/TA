111d5aac7e0c578b69989dd57c3a338c7e583512
nixtux <davehomer@googlemail.com>
2017-08-20 07:56:35 +0100

    Add wip t1.5 air

---
+++ weapons/blastwing.lua
@@ -0,0 +1,21 @@
+return {
+	blastwing = {
+		areaofeffect = 150,
+		craterboost = 0,
+		cratermult = 0,
+		edgeeffectiveness = 0.75,
+		explosiongenerator = "custom:EMPFLASH240",
+		impulseboost = 0,
+		impulsefactor = 0,
+		model = "bomb",
+		name = "EMPboom",
+		paralyzer = true,
+		paralyzetime = 12,
+		soundhit = "EMGPULS1",
+		soundstart = "bombrel",
+		damage = {
+			commanders = 3,
+			default = 120,
+		},
+	},
+}
