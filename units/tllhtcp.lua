8aa050dc11738804c7f3aaf2c23f1c3bf0b87088
skymyj <skymyj@hotmail.com>
2017-08-22 14:59:18 +0200

    Update tllhtcp.lua

--- units/tllhtcp.lua
+++ units/tllhtcp.lua
@@ -49,7 +49,7 @@ return {
 		workertime = 980,
 		yardmap = "ooccccccoo ooccccccoo ooccccccoo cccccccccc cccccccccc cccccccccc ooccccccoo ooccccccoo ooccccccoo ooccccccoo ",
 		buildoptions = {
-			[1] = "tllbuilderlvl3",
+			[1] = "tlldoc",
 			[2] = "tllblind",
 			[3] = "heavyimpact",
 			[4] = "tllhtml",
