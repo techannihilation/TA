f4aab50582dbad33c8c9c955a0f92c26fffa94e9
skymyj <skymyj@hotmail.com>
2017-09-04 15:45:21 +0200

    Update fatshrew.lua

--- units/fatshrew.lua
+++ units/fatshrew.lua
@@ -39,7 +39,7 @@ return {
 		maxvelocity = 1.4,
 		maxwaterdepth = 520,
 		mobilestandorders = 1,
-		movementclass = "hkbot4",
+		movementclass = "HKBOT7",
 		name = "Fatshrew Mech",
 		noautofire = false,
 		nochasecategory = "SUB VTOL",
