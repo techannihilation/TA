916e29ae945d478a7cb0b03bdd053bc1f7420419
skymyj <skymyj@hotmail.com>
2017-09-07 16:11:32 +0200

    Update tllacv.lua

--- units/tllacv.lua
+++ units/tllacv.lua
@@ -71,8 +71,7 @@ return {
 			[16] = "tllmds",
 			[17] = "tllvp",
 			[18] = "tllavp",
-			[19] = "tllevp",
-			[20] = "tllnanotc1",			
+			[19] = "tllevp",			
 		},
 		customparams = {
 			faction = "TLL",
