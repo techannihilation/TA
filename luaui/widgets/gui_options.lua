4ada1f6f6615f7b17eb90d5d6e017dd026ba5aa2
nixtux <davehomer@googlemail.com>
2017-09-11 14:53:54 +0100

    Update gui_options.lua

--- luaui/widgets/gui_options.lua
+++ luaui/widgets/gui_options.lua
@@ -939,6 +939,7 @@ function applyOptionValue(i, skipRedrawWindow)
 				WG["background_color_over"] = (value*0.01)
 			end
 		elseif id == 'musicvolume' then
+		Spring.SetSoundStreamVolume(value)
 			WG["music_volume"] = value
 		elseif id == 'highlightselunits_opacity' then
 			WG['highlightselunits'].setOpacity(value)
