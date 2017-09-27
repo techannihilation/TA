95810814d941dc657ab8fe81f75ca9c8cfcfd775
skymyj <skymyj@hotmail.com>
2017-09-27 21:32:26 +0200

    Balance mortor explosion

--- luarules/configs/area_damage_defs.lua
+++ luarules/configs/area_damage_defs.lua
@@ -15,9 +15,9 @@ local weapons = {
 	tlljuno_juno_pulse = { radius = 950, damage = 5, duration = 90, rangeFall = 0.8, timeFall = 0.1, scoutScale = 10},
 	
 	--mortor_canon
-	amortor_mortor_cannon =  { radius = 192, damage = 200, duration = 120, rangeFall = 0.25, timeFall = 0.25, allyScale = 0.25, teamScale = 0.25},
-	cmortor_mortor_cannon =  { radius = 192, damage = 200, duration = 120, rangeFall = 0.25, timeFall = 0.25, allyScale = 0.25, teamScale = 0.25},
-	tllmortor_mortor_cannon =  { radius = 192, damage = 200, duration = 120, rangeFall = 0.25, timeFall = 0.25, allyScale = 0.25, teamScale = 0.25},
+	amortor_mortor_cannon =  { radius = 95, damage = 200, duration = 120, rangeFall = 0.25, timeFall = 0.5, allyScale = 0.25, teamScale = 0.25},
+	cmortor_mortor_cannon =  { radius = 95, damage = 200, duration = 120, rangeFall = 0.25, timeFall = 0.5, allyScale = 0.25, teamScale = 0.25},
+	tllmortor_mortor_cannon =  { radius = 95, damage = 200, duration = 120, rangeFall = 0.25, timeFall = 0.5, allyScale = 0.25, teamScale = 0.25},
 
 }
 
