b87ddef3226cd43304660c2d14c7474999355c44
skymyj <skymyj@hotmail.com>
2017-11-04 21:38:24 +0100

    Update cormoho.lua

--- units/cormoho.lua
+++ units/cormoho.lua
@@ -31,7 +31,7 @@ return {
 		maxslope = 20,
 		maxvelocity = 0,
 		maxwaterdepth = 20,
-		metalstorage = 1000,
+		metalstorage = 250,
 		name = "Moho Mine",
 		noautofire = false,
 		nochasecategory = "ALL",
