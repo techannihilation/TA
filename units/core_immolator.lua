65fda42137783df715de7225f544fa64f18b9986
skymyj <skymyj@hotmail.com>
2017-11-13 11:35:25 +0100

    Update core_immolator.lua

--- units/core_immolator.lua
+++ units/core_immolator.lua
@@ -42,7 +42,7 @@ return {
 		objectname = "core_immolator",
 		radardistance = 0,
 		radaremitheight = 30,
-		selfdestructas = "ATOMIC_BLAST",
+		selfdestructas = "CRAWL_BLAST",
 		sightdistance = 665,
 		standingfireorder = 2,
 		unitname = "core_immolator",
