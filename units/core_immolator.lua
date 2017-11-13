8b7c2ca608d1df8084a679ec2d52a09930762e37
skymyj <skymyj@hotmail.com>
2017-11-13 10:47:35 +0100

    Update core_immolator.lua

--- units/core_immolator.lua
+++ units/core_immolator.lua
@@ -9,7 +9,7 @@ return {
 		buildinggrounddecalsizey = 6,
 		buildinggrounddecaltype = "core_immolator_aoplane.dds",
 		buildpic = "core_immolator.dds",
-		buildtime = 45000,
+		buildtime = 30000,
 		canattack = true,
 		canguard = true,
 		canstop = 1,
