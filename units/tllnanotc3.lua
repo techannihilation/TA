1a05d9d3fd5e268bfa7695215bc1c7299724e8ca
skymyj <skymyj@hotmail.com>
2017-11-28 14:48:52 +0100

    Update tllnanotc3.lua

--- units/tllnanotc3.lua
+++ units/tllnanotc3.lua
@@ -53,7 +53,7 @@ return {
 		unitname = "tllnanotc3",
 		upright = true,
 		usebuildinggrounddecal = true,
-		workertime = 1500,
+		workertime = 1600,
 		customparams = {
 			buildpic = "tllnanotc3.png",
 			faction = "TLL",
