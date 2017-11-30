a66498ad1fe9d5435df566637c51d0e66d2f8684
skymyj <skymyj@hotmail.com>
2017-11-30 19:20:29 +0100

    Update tllhailstorm.lua

--- units/tllhailstorm.lua
+++ units/tllhailstorm.lua
@@ -28,7 +28,7 @@ return {
 		maxvelocity = 0.75,
 		maxwaterdepth = 12,
 		mobilestandorders = 1,
-		movementclass = "hkbot4",
+		movementclass = "VKBOT7",
 		name = "Hailstorm",
 		noautofire = false,
 		nochasecategory = "SUB VTOL",
