11e79d04fed71bfe16b242a581d85bc5e93138d4
nixtux <davehomer@googlemail.com>
2017-12-04 12:07:19 +0000

    Update unit_airbase.lua

--- luarules/gadgets/unit_airbase.lua
+++ luarules/gadgets/unit_airbase.lua
@@ -448,6 +448,7 @@ function gadget:GameFrame(n)
          local airbaseID, padPieceNum = t[1], t[2]
          local px, py, pz = Spring.GetUnitPiecePosDir(airbaseID, padPieceNum)
          local ux, uy, uz = Spring.GetUnitPosition(unitID)
+		 if ux = nil or uy = nil or uz = nil then RemoveTractorPlane(unitID) return end		
          local sqrDist = (ux and px) and (ux-px)^2 + (uy-py)^2 + (uz-pz)^2
          if sqrDist and h<mh then
             -- check if we're close enough, move into tractorPlanes if so
