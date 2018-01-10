501ab60c009c5d4beccf05157a996741a104d71c
skymyj <skymyj@hotmail.com>
2018-01-10 12:14:55 +0100

    Update tllfflak.lua

--- units/tllfflak.lua
+++ units/tllfflak.lua
@@ -114,7 +114,7 @@ return {
 				impulsefactor = 0,
 				name = "FlakCannon",
 				noselfdamage = true,
-				range = 930,
+				range = 1020,
 				reloadtime = 0.38,
 				rgbcolor = "1.0 0.5 0.0",
 				size = 5,
