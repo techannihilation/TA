a79ec82bdea3ae5315ca28f9c8c2f9be87e30bb0
ksilversurfer <35111293+ksilversurfer@users.noreply.github.com>
2018-01-15 09:54:55 +0100

    Fix Typo 2

--- luarules/gadgets/unit_air_release_manager.lua
+++ luarules/gadgets/unit_air_release_manager.lua
@@ -14,7 +14,7 @@ if (not gadgetHandler:IsSyncedCode()) then
   return
 end
 
-local paratroopers = {
+local paraTroopers = {
 		[UnitDefNames["commando"].id] = true,
 		[UnitDefNames["armmav"].id] = true
 	}
