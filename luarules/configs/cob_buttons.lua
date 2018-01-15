05cbc3dbc43cd1ae3e71a103f05e116c981319cf
ksilversurfer <35111293+ksilversurfer@users.noreply.github.com>
2018-01-15 21:50:31 +0100

    Update cob_buttons.lua

--- luarules/configs/cob_buttons.lua
+++ luarules/configs/cob_buttons.lua
@@ -89,6 +89,15 @@ return {
 		params = {'1', 'Shield Off', 'Shield On'},
 	},
   },
+  armsmc = {
+	{
+		name = "shield_smc",
+		cob = "Shield",
+		tooltip = "Toggle shield activation",
+		type = CMDTYPE.ICON_MODE,
+		params = {'1', 'Shield Off', 'Shield On'},
+	},
+  },
   corcom5 = {
 	{
 		name = "shield_cc5",
