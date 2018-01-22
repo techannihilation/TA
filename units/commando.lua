210c2f29f3d35b627246d7071bbd000f4237dafc
ksilversurfer <35111293+ksilversurfer@users.noreply.github.com>
2018-01-22 11:27:20 +0100

    Update commando.lua

--- units/commando.lua
+++ units/commando.lua
@@ -23,7 +23,7 @@ return {
 		collisionvolumescales = "24 45 20",
 		collisionvolumetype = "Box",
 		defaultmissiontype = "Standby",
-		description = "Paratrooping Stealthy Capture Kbot",
+		description = "Paratrooping Stealthy Fast Capture Kbot (Paralysis Resistant)",
 		explodeas = "SPYBOMBX",
 		firestandorders = 0,
 		footprintx = 2,
