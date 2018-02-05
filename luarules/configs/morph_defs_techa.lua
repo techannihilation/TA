79549e9da1e790b0eab322e443db4b1c6c4e39a4
skymyj <skymyj@hotmail.com>
2018-02-05 14:08:02 +0100

    Add Chimera for Lighting Tank Tree Morph (#839)
    * Update morph_defs_techa.lua
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update morph_defs_techa.lua

--- luarules/configs/morph_defs_techa.lua
+++ luarules/configs/morph_defs_techa.lua
@@ -552,9 +552,14 @@ local morphDefs = {
 		xp = 0.05,
 	},	
 	armlatnk = {
+		into = 'armchimera',
+		time = 60,
+		xp = 0.50,
+	},
+	armchimera = {
 		into = 'nsaagriz',
-		time = 180,
-		xp = 1.25,
+		time = 120,
+		xp = 0.75,
 	},
 	armstump = {
 		into = 'armstump1',
