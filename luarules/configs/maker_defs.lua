6abb4099e44b9849fde5aacc6aabc3f2845ef648
skymyj <skymyj@hotmail.com>
2018-02-19 10:17:13 +0100

    Balance metalmakerlvl2 2/2 (#882)
    * Update ametalmakerlvl2.lua
    
    * Update cmetalmakerlvl2.lua
    
    * Update cmetalmakerlvl3.lua
    
    * Update ametalmakerlvl3.lua
    
    * Update tllmetalmakerlvl3.lua
    
    * Update maker_defs.lua
    
    * Update maker_defs.lua
    
    * Update cmetalmakerlvl3.lua
    
    * Update ametalmakerlvl3.lua
    
    * Update cmetalmakerlvl3.lua
    
    * Add files via upload
    
    * Update maker_defs.lua
    
    * Update maker_defs.lua

--- luarules/configs/maker_defs.lua
+++ luarules/configs/maker_defs.lua
@@ -3,12 +3,12 @@ local ACZ = 1.0/100
 local AC0 = 1.2/100
 local AC1 = 1.3/100
 local AC2 = 1.4/100
-local A3 = 1.50/100
+local A3 = 1.48/100
 local C3 = 1.46/100
-local AC4 = 1.6/100
-local AC5 = 1.7/100
-local AC6 = 1.8/100
-local AC7 = 1.9/100
+local AC4 = 1.50/100
+local AC5 = 1.55/100
+local AC6 = 1.6/100
+local AC7 = 1.7/100
 
 
 local T0 = 1.08/100
@@ -17,7 +17,7 @@ local T1 = 1.16/100
 --local T3 = 1.31/100
 local T4 = 1.42/100
 local T5 = 1.47/100
-local T6 = 1.75/100
+local T6 = 1.6/100
 
 
 local convertCapacities = {
@@ -30,8 +30,8 @@ local convertCapacities = {
 		[UnitDefNames.armckmakr.id]  = { c = 400, e = (A3) }, -- Cloaked/Hardened
 		[UnitDefNames.armmmkr.id]  = { c = 800, e = (AC4) }, -- Moho
 		[UnitDefNames.armuwmmm.id]  = { c = 1000, e = (AC5) }, -- Underwater
-		[UnitDefNames.ametalmakerlvl2.id]  = { c = 16000, e = (AC6) }, -- T3
-		[UnitDefNames.ametalmakerlvl3.id]  = { c = 80000, e = (AC7) }, -- T4
+		[UnitDefNames.ametalmakerlvl2.id]  = { c = 8000, e = (AC6) }, -- T3
+		[UnitDefNames.ametalmakerlvl3.id]  = { c = 75000, e = (AC7) }, -- T4
 		
 		[UnitDefNames.armgen.id]  = { c = UnitDefNames.armgen.energyMake, e = (ACZ) }, -- Armgen Combo fusion
 					
@@ -43,8 +43,8 @@ local convertCapacities = {
 		[UnitDefNames.corhmakr.id]  = { c = 3072, e = (C3) }, -- Cloaked/Hardened
 		[UnitDefNames.cormmkr.id]  = { c = 1024, e = (AC4) }, -- Moho
 		[UnitDefNames.coruwmmm.id]  = { c = 1280, e = (AC5) }, -- Underwater
-		[UnitDefNames.cmetalmakerlvl2.id]  = { c =20480, e = (AC6) }, -- T3
-		[UnitDefNames.cmetalmakerlvl3.id]  = { c = 90000, e = (AC7) }, -- T4
+		[UnitDefNames.cmetalmakerlvl2.id]  = { c = 10240, e = (AC6) }, -- T3
+		[UnitDefNames.cmetalmakerlvl3.id]  = { c = 60000, e = (AC7) }, -- T4
 
 		[UnitDefNames.corgen.id]  = { c = UnitDefNames.corgen.energyMake, e = (ACZ) }, -- Corgen Combo fusion
 		--TLL
@@ -54,7 +54,7 @@ local convertCapacities = {
 		--[UnitDefNames.____.id]  = { c = 600, e = (T3) }, -- Cloaked/Hardened
 		[UnitDefNames.tllammaker.id]  = { c = 1000, e = (T4) }, -- Moho
 		[UnitDefNames.tllwmmohoconv.id]  = { c = 1000, e = (T5) }, -- Underwater
-		[UnitDefNames.tllmetalmakerlvl3.id]  = { c = 60000, e = (T6) }, -- T4
+		[UnitDefNames.tllmetalmakerlvl3.id]  = { c = 30000, e = (T6) }, -- T4
 		--[UnitDefNames.____.id]  = { c = 10000, e = (T6) }, -- Super
 
 		[UnitDefNames.tllgen.id]  = { c = UnitDefNames.tllgen.energyMake, e = (ACZ) }, -- Corgen Combo fusion
