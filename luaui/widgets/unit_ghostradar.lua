36298ee3eb114edf77f64d6e79bed7df39cb76d6
nixtux <davehomer@googlemail.com>
2018-03-03 08:25:33 +0000

    Update ghostradar

--- luaui/widgets/unit_ghostradar.lua
+++ luaui/widgets/unit_ghostradar.lua
@@ -19,22 +19,23 @@ local lineWidth = 1.0 -- calcs dynamic now
 local printDebug
 
 local udefTab				= UnitDefs
-local spGetUnitDefID        = Spring.GetUnitDefID
-local spEcho                = Spring.Echo
-local spGetUnitPosition     = Spring.GetUnitPosition
-local spGetGameSeconds      = Spring.GetGameSeconds
-local floor                 = math.floor
+local spGetUnitDefID		= Spring.GetUnitDefID
+local spEcho				= Spring.Echo
+local spGetUnitPosition		= Spring.GetUnitPosition
+local spGetGameSeconds		= Spring.GetGameSeconds
+local floor					= math.floor
 local pairs					= pairs
-local spGetMyPlayerID       = Spring.GetMyPlayerID
-local spGetPlayerInfo       = Spring.GetPlayerInfo
+local spGetMyPlayerID		= Spring.GetMyPlayerID
+local spGetPlayerInfo		= Spring.GetPlayerInfo
 local spIsUnitInView		= Spring.IsUnitInView
 
-local glColor               = gl.Color
-local glDepthTest           = gl.DepthTest
+local glColor				= gl.Color
+local glDepthTest			= gl.DepthTest
 local glUnitShape			= gl.UnitShape
-local glPopMatrix           = gl.PopMatrix
-local glPushMatrix          = gl.PushMatrix
-local glTranslate           = gl.Translate
+local glPopMatrix			= gl.PopMatrix
+local glPushMatrix			= gl.PushMatrix
+local glTranslate			= gl.Translate
+local glLoadIdentity		= gl.LoadIdentity
 ----------------------------------------------------------------
 
 local debug = false
@@ -46,9 +47,6 @@ local TooHigh = false
 local HighPing = false
 
 function widget:Initialize()
-	if Spring.IsReplay() or Spring.GetGameFrame() > 0 then
-	    widget:PlayerChanged()
-  	end
 	widgetHandler:RegisterGlobal('DrawManager_ghost', DrawStatus)
 end
 
@@ -57,9 +55,9 @@ function widget:Shutdown()
 end
 
 function DrawStatus(toohigh,fps,ping)
-    --Spring.Echo(toohigh,fps,ping)
-    TooHigh = toohigh
-    HighPing = ping
+	--Spring.Echo(toohigh,fps,ping)
+	TooHigh = toohigh
+	HighPing = ping
 end
 
 function widget:UnitEnteredRadar(unitID, allyTeam)
@@ -84,7 +82,6 @@ function widget:UnitEnteredLos(unitID, allyTeam )
 		dots[unitID] = nil	
 	end
 end
-
 
 function widget:UnitCreated(unitID, allyTeam)
 	--kill the dot info if this unitID gets reused on own team
@@ -111,40 +108,44 @@ function widget:UnitLeftLos(unitID, allyTeam)
 	end
 end
 
-
 function widget:DrawWorld()
 	--Spring.Echo(TooHigh,HighPing)
 	if TooHigh or HighPing then 
 		return
 	end
-	
 	glColor(1.0, 1.0, 1.0, 0.35 )
 	glDepthTest(true)
-
 	for unitID, dot in pairs( dots ) do
 		if ( dot["radar"] == true ) and ( dot["los"] == false ) and ( dot["unitDefId"] ~= nil ) then
 			local x, y, z = spGetUnitPosition(unitID)
 			if x and ( spIsUnitInView(unitID) ) then
 				glPushMatrix()
+				glLoadIdentity()
 				glTranslate( x, y + 5 , z)
-				glUnitShape( dot["unitDefId"], dot["teamId"], false, true, false)					      
+				glUnitShape( dot["unitDefId"], dot["teamId"], false, false, false)					      
 				glPopMatrix()
 			end
 		end
 	end
-
 	glDepthTest(false)
  	glColor(1, 1, 1, 1)
 end
 
-function widget:PlayerChanged(playerID)
-	if Spring.GetSpectatingState() and Spring.GetGameFrame() > 0 then
-		widgetHandler:RemoveWidget(self)
+function widget:Update()
+	local timef = spGetGameSeconds()
+	local time = floor(timef)
+	-- update timers once every <updateInt> seconds
+	if (time % updateInt == 0 and time ~= lastTime) then	
+		lastTime = time
+		--do update stuff:
+		local playerID = spGetMyPlayerID()
+		local _, _, spec, _, _, _, _, _ = spGetPlayerInfo(playerID)
+		if ( spec == true ) then
+			spEcho("<GhostRadar> Spectator mode. Widget removed.")
+			widgetHandler:RemoveWidget(self)
+			return false
+		end
 	end
-end
-
-function widget:GameStart()
-	widget:PlayerChanged()
 end
 
 function printDebug( value )
