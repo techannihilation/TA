044c18023f6db37a8cbf65a0b77eb73f89db1ca1
ksilversurfer <35111293+ksilversurfer@users.noreply.github.com>
2018-03-22 15:07:38 +0100

    Fix Ecostats spring crash

--- luaui/widgets/gui_ecostats.lua
+++ luaui/widgets/gui_ecostats.lua
@@ -969,10 +969,10 @@ function getTeamProduction(teamID)
 	local currentWind = string.format('%.1f', select(4,Spring.GetWind()))
 
 	if inSpecMode then
-		local teamUnits = Spring.GetTeamUnitsSorted(teamID)
-		for unitDefID, units in pairs(teamUnits) do
+		local teamUnits = Spring.GetTeamUnits(teamID)
+		for unitID in pairs(teamUnits) do
+			unitDefID = Spring.GetUnitDefID(unitID)
 			if reclaimerUnitDefs[unitDefID] then -- or metalUnitDefs[unitDefID]
-				for i, unitID in ipairs(units) do
 					local metalMake,metalUse,energyMake,energyUse = Spring.GetUnitResources(unitID)
 					if metalMake > 0 then
 						totalMetalReclaim = totalMetalReclaim + (metalMake-reclaimerUnitDefs[unitDefID][1])
@@ -980,7 +980,6 @@ function getTeamProduction(teamID)
 					if energyMake > 0 then
 						totalEnergyReclaim = totalEnergyReclaim + (energyMake-reclaimerUnitDefs[unitDefID][2])
 					end
-				end
 			end
 		end
 	end
