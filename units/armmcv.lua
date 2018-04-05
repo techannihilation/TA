69408502ba575700e4c31841c0f0b04dfa768358
skymyj <skymyj@hotmail.com>
2018-04-05 14:41:25 +0200

    Update armmcv.lua

--- units/armmcv.lua
+++ units/armmcv.lua
@@ -85,6 +85,9 @@ return {
 			},
 		},
 		sfxtypes = {
+			explosiongenerators = {
+				[1] = "custom:MEDIUMFLARE",
+			},
 			pieceexplosiongenerators = {
 				[1] = "piecetrail0",
 				[2] = "piecetrail1",
