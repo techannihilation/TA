263b38daff53e9e2f272bc80c8e32ad421473545
skymyj <skymyj@hotmail.com>
2018-04-05 14:40:47 +0200

    Update cormcv.lua

--- units/cormcv.lua
+++ units/cormcv.lua
@@ -84,6 +84,9 @@ return {
 			},
 		},
 		sfxtypes = {
+			explosiongenerators = {
+				[1] = "custom:MEDIUMFLARE",
+			},
 			pieceexplosiongenerators = {
 				[1] = "piecetrail0",
 				[2] = "piecetrail1",
