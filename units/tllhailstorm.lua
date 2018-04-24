191078a9dca42c16135b38e4893165afc32f3bcd
skymyj <skymyj@hotmail.com>
2018-04-24 19:41:07 +0200

    Update tllhailstorm.lua

--- units/tllhailstorm.lua
+++ units/tllhailstorm.lua
@@ -120,7 +120,7 @@ return {
 				cegtag = "vulcanfx_lowp",
 				craterboost = 0,
 				cratermult = 0,
-				energypershot = 3000,
+				energypershot = 2500,
 				explosiongenerator = "custom:BERTHASHOT_LOWP",
 				impulseboost = 0,
 				impulsefactor = 0,
