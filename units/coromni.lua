10a06936bfc860b2a0481493f2ab984ad8e3b438
skymyj <skymyj@hotmail.com>
2018-05-07 13:13:38 +0200

    Update coromni.lua

--- units/coromni.lua
+++ units/coromni.lua
@@ -8,7 +8,7 @@ return {
 		buildinggrounddecaldecayspeed = 30,
 		buildinggrounddecalsizex = 8,
 		buildinggrounddecalsizey = 8,
-		buildinggrounddecaltype = "coromni_aoplane.dds",
+		buildinggrounddecaltype = "corarad_aoplane.dds",
 		buildpic = "coromni.dds",
 		buildtime = 125000,
 		category = "ALL NOTDEFENSE NOTHOVERNOTVTOL NOTMOBILE NOTSUB NOTSUBNOTSHIP NOTVTOL NOTWEAPON SURFACE",
