79bd85696522fb9ea129e68ffc2da482a5297114
skymyj <skymyj@hotmail.com>
2018-06-01 15:36:56 +0200

    Update armhplasma.lua

--- units/armhplasma.lua
+++ units/armhplasma.lua
@@ -83,6 +83,9 @@ return {
 			},
 		},
 		sfxtypes = {
+			explosiongenerators = {
+				[1] = "custom:MEDIUMFLARE",
+			},
 			pieceexplosiongenerators = {
 				[1] = "piecetrail0",
 				[2] = "piecetrail1",
