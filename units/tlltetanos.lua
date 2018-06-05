2813d570a5111f2ac8971a1f9e517190cfff1465
skymyj <skymyj@hotmail.com>
2018-06-05 14:09:26 +0200

    Update tlltetanos.lua

--- units/tlltetanos.lua
+++ units/tlltetanos.lua
@@ -120,7 +120,7 @@ return {
 		},
 		weapondefs = {
 			tll_missile_aa = {
-				areaofeffect = 32,
+				areaofeffect = 48,
 				avoidfeature = false,
 				burnblow = true,
 				canattackground = false,
