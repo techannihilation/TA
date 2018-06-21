69f493132edb81093360bbbe54ea09b67755fc26
skymyj <skymyj@hotmail.com>
2018-06-21 15:12:51 +0200

    Update corbsub.lua

--- units/corbsub.lua
+++ units/corbsub.lua
@@ -4,7 +4,7 @@ return {
 		activatewhenbuilt = true,
 		brakerate = 0.057,
 		buildcostenergy = 124265,
-		buildcostmetal = 8400,
+		buildcostmetal = 8950,
 		builder = false,
 		buildpic = "corbsub.dds",
 		buildtime = 110000,
