de036fe56062103a1df28b3fb0eb8dd88b9e96b8
oxide <647338+nixtux@users.noreply.github.com>
2018-08-06 07:01:02 +0100

    Disharge helper widget, draw aoe also

--- luaui/widgets/cmd_discharge_helper.lua
+++ luaui/widgets/cmd_discharge_helper.lua
@@ -10,9 +10,16 @@ function widget:GetInfo()
   }
 end
 
+local glDrawGroundCircle = gl.DrawGroundCircle
+local glColor = gl.Color
+local glLineWidth = gl.LineWidth
+
 local empID = 105
 local dischargeID = UnitDefNames["tlldischarge"].id
 local pressed = false
+local pos = {}
+local dischargeSelected = false
+local empAOE = WeaponDefs[UnitDefs[dischargeID].weapons[2].weaponDef].damageAreaOfEffect
 
 function widget:KeyPress(key)
     --Spring.Echo(key)
@@ -22,15 +29,37 @@ function widget:KeyPress(key)
 end
 
 function widget:KeyRelease(key)
-    local selectedUnits = Spring.GetSelectedUnits()
-    if #selectedUnits == 1 and pressed then
-        selectedunitID = selectedUnits[1]
-        if Spring.GetUnitDefID(selectedUnits[1]) == dischargeID then
-            local mx,my = Spring.GetMouseState()
-            local _,pos = Spring.TraceScreenRay(mx,my,true)
-            --Spring.Echo(key,dischargeID,pos[1],pos[2],pos[3])
-            Spring.GiveOrderToUnit(selectedunitID, empID, pos, {})
-        end
+    if dischargeSelected and key == 101 then
+        Spring.GiveOrderToUnit(selectedunitID, empID, pos, {})
     end
     pressed = false
+end
+
+function widget:Update()
+    if pressed then
+        local selectedUnits = Spring.GetSelectedUnits()
+        if #selectedUnits == 1 and pressed then
+            selectedunitID = selectedUnits[1]
+            if Spring.GetUnitDefID(selectedUnits[1]) == dischargeID then
+                dischargeSelected = true
+                local mx,my = Spring.GetMouseState()
+                _, pos = Spring.TraceScreenRay(mx,my,true)
+            else
+                dischargeSelected = false
+            end
+        else
+            dischargeSelected = false
+        end
+    else
+        dischargeSelected = false
+    end
+end
+
+function widget:DrawWorld()
+    if dischargeSelected then
+        glColor(1, 1, 0, 0.75)
+        glLineWidth(1)
+        glDrawGroundCircle(pos[1], pos[2], pos[3], empAOE, 64)
+        glColor(1,1,1,1)
+    end
 end
