dc5994e0d486cdb3176d43d1c99a9deb6b582d44
oxide <647338+nixtux@users.noreply.github.com>
2018-08-09 21:20:52 +0100

    All tidal generators speed now depends on tidal speed, Closes #299

--- luarules/gadgets/unit_tidal_speed_control.lua
+++ luarules/gadgets/unit_tidal_speed_control.lua
@@ -4,12 +4,12 @@
 function gadget:GetInfo()
   return {
     name      = "COB Tidal Speed Control",
-    desc      = "Sends Tidal speed to scripts",
+    desc      = "Sends Tidal speed to COB scripts",
     author    = "Nixtux",
-    date      = "Aug 13, 2013",
+    date      = "Aug 9, 2018",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = false    --  loaded by default?
+    enabled   = true    --  loaded by default?
   }
 end
 
@@ -23,29 +23,14 @@ end
 -------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------
 
-local rockideDefs = {
-    [UnitDefNames['tlluwjam'].id ] = true,
+local tidalDefs = {
+    [UnitDefNames['armatidal'].id ] = true,
+    [UnitDefNames['armtide'].id ] = true,
+    [UnitDefNames['coratidal'].id ] = true,
+    [UnitDefNames['cortide'].id ] = true,
+    [UnitDefNames['tllatidal'].id ] = true,
     [UnitDefNames['tllsolarns'].id ] = true,
-    [UnitDefNames['tllsy'].id ] = true,
-    [UnitDefNames['tllasy'].id ] = true,
-    [UnitDefNames['tllplat'].id ] = true,
-    [UnitDefNames['tllhpns'].id ] = true,
-    [UnitDefNames['tllwmconv'].id ] = true,
-    [UnitDefNames['tlluwjam'].id ] = true,
-    [UnitDefNames['tlltorp'].id ] = true,
     [UnitDefNames['tlltide'].id ] = true,
-    [UnitDefNames['tllsonar'].id ] = true,
-    [UnitDefNames['tllnssam'].id ] = true,
-    [UnitDefNames['tllasonar'].id ] = true,
-    [UnitDefNames['tlllmtns'].id ] = true,
-    [UnitDefNames['tllhltns'].id ] = true,
-    [UnitDefNames['tlldcsta'].id ] = true,
-    [UnitDefNames['tllatorp'].id ] = true,
-    [UnitDefNames['tllfgate'].id ] = true,
-    [UnitDefNames['tllfflak'].id ] = true,
-    [UnitDefNames['tllwmmohoconv'].id ] = true,
-    [UnitDefNames['tlltrid'].id ] = true,
-    [UnitDefNames['tllkrak'].id ] = true,
 }
 
 -------------------------------------------------------------------------------------
@@ -53,15 +38,20 @@ local rockideDefs = {
 
 -- Speed-ups
 local SetUnitCOBValue      = Spring.SetUnitCOBValue
-local GetTide              = Game.tidal/10
+local GetTide              = Game.tidal*1.5
 
 -------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------
 
 function gadget:UnitCreated(unitID, unitDefID, unitTeam)
-	if rockideDefs[unitDefID] then
-		--Spring.Echo("TIDAL SPEED :" .. GetTide)
-		SetUnitCOBValue(unitID,1030,GetTide)
+	if tidalDefs[unitDefID] then
+    	local uDef = UnitDefs[unitDefID]
+    	local mult = 1
+    	if uDef.customParams and uDef.customParams.energymultiplier then
+       		mult = uDef.customParams.energymultiplier*.75 or 1
+    	end
+		--Spring.Echo("TIDAL SPEED :" .. GetTide*mult)
+		Spring.CallCOBScript(unitID, "TidalSpeed", 0, GetTide*mult)
 	end
 end
 
