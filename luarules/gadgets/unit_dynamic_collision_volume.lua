4c54ff7be4bedefef88ef5132277a13f82fb5e2c
oxide <647338+nixtux@users.noreply.github.com>
2018-11-15 15:54:06 +0000

    Reduce unit radius for corshock

--- luarules/gadgets/unit_dynamic_collision_volume.lua
+++ luarules/gadgets/unit_dynamic_collision_volume.lua
@@ -156,7 +156,7 @@ if (gadgetHandler:IsSyncedCode()) then
 				elseif (not canFly[unitDefID] ) then
 					rs, hs, ws = 0.68, 0.68, 0.68
 					ars, ahs = 0.70, 0.70 
-				elseif spGetUnitRadius(unitID)>150 then --Fix hero's not moving in latest engine
+				elseif spGetUnitRadius(unitID)>120 then --Fix hero's not moving in latest engine
 					--Spring.Echo(UnitDefs[unitDefID].name,spGetUnitRadius(unitID))
 					rs, hs, ws = 0.77, 0.18, 0.77
 					ars, ahs = 0.25, 0.25 
