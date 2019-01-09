d7a0bf11fa681f6159fd325d8fb0544625af3cf2
ksilversurfer <gataka7845>
2019-01-09 03:21:32 +0100

    Update unit_prevent_nuke_share.lua

--- luarules/gadgets/unit_prevent_nuke_share.lua
+++ luarules/gadgets/unit_prevent_nuke_share.lua
@@ -14,20 +14,25 @@ end
 --------------------------------------------------------------------------------
 -- SPEEDUPS
 --------------------------------------------------------------------------------
-local color = "\255\255\64\64"
 local SendMessageToTeam = Spring.SendMessageToTeam
-
+local GetTeamUnits = Spring.GetTeamUnits
+local GetUnitDefID = Spring.GetUnitDefID
+local GetTeamList = Spring.GetTeamList
 --------------------------------------------------------------------------------
 -- CONSTANS
 --------------------------------------------------------------------------------
-local NUKES = {
-	[UnitDefNames.armsilo.id] = UnitDefNames.armsilo.humanName,
-	[UnitDefNames.armsilo1.id] = UnitDefNames.armsilo1.humanName,
-	[UnitDefNames.corsilo.id] = UnitDefNames.corsilo.humanName,
-	[UnitDefNames.corsilo1.id] = UnitDefNames.corsilo1.humanName,
-	[UnitDefNames.tllsilo.id] = UnitDefNames.tllsilo.humanName,
-	[UnitDefNames.tllsilo1.id] = UnitDefNames.tllsilo1.humanName
-}
+local NukeList = {"armsilo", "armsilo1", "corsilo", "corsilo1", "tllsilo", "tllsilo1"}
+local NUKES = {}
+
+for _, v in pairs(NukeList) do
+	NUKES[UnitDefNames[v].id] = {
+		name = UnitDefNames[v].humanName,
+		maxThisUnit = UnitDefNames[v].maxThisUnit
+	}
+end
+
+local TeamNukesNb = {}
+local color = "\255\255\64\64"
 
 --------------------------------------------------------------------------------
 -- START OF CODE
@@ -36,13 +41,46 @@ if (gadgetHandler:IsSyncedCode()) then
 	--------------------------------------------------------------------------------
 	-- BEGIN SYNCED
 	--------------------------------------------------------------------------------
-	function gadget:AllowUnitTransfer(unitID, unitDefID, oldTeam, newTeam, capture)
-		if NUKES[unitDefID] then
-			SendMessageToTeam(oldTeam, color .. "Warning: " .. NUKES[unitDefID] .. " can not be shared to other team !")
+	function gadget:Initialize()
+		for _, teamID in ipairs(GetTeamList()) do
+			TeamNukesNb[teamID] = 0
+			AllTeamUnits = GetTeamUnits(teamID)
 
+			for i = 1, #AllTeamUnits do
+				local unitID = AllTeamUnits[i]
+				local unitDefID = GetUnitDefID(unitID)
+
+				if NUKES[unitDefID] then
+					TeamNukesNb[teamID] = TeamNukesNb[teamID] + 1
+				end
+			end
+		end
+	end
+
+	function gadget:UnitCreated(unitID, unitDefID, unitTeam)
+		if NUKES[unitDefID] then
+			TeamNukesNb[unitTeam] = TeamNukesNb[unitTeam] + 1
+		end
+	end
+
+	function gadget:UnitDestroyed(unitID, unitDefID, unitTeam)
+		if NUKES[unitDefID] then
+			TeamNukesNb[unitTeam] = TeamNukesNb[unitTeam] - 1
+		end
+	end
+
+	function gadget:UnitGiven(unitID, unitDefID, newTeam, oldTeam)
+		if NUKES[unitDefID] then
+			TeamNukesNb[oldTeam] = TeamNukesNb[oldTeam] - 1
+			TeamNukesNb[newTeam] = TeamNukesNb[newTeam] + 1
+		end
+	end
+
+	function gadget:AllowUnitTransfer(unitID, unitDefID, oldTeam, newTeam, capture)
+		if NUKES[unitDefID] and (TeamNukesNb[newTeam] >= NUKES[unitDefID].maxThisUnit - 1) then
+			SendMessageToTeam(oldTeam, color .. "Warning: Can't share " .. NUKES[unitDefID].name .. ", nuke number limit for team " .. newTeam .. " has been reached.")
 			return false
 		end
-
 		return true
 	end
 else
