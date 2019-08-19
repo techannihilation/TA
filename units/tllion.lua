c68bea6da3ac462de31453f77fe2a3b6b5114e8b
skymyj <skymyj@hotmail.com>
2019-08-19 20:22:27 +0200

    Update tllion.lua

--- units/tllion.lua
+++ units/tllion.lua
@@ -9,7 +9,7 @@ return {
 		buildinggrounddecalsizey = 19,
 		buildinggrounddecaltype = "tllion_aoplane.dds",
 		buildpic = "tllion.dds",
-		buildtime = 1559848,
+		buildtime = 2000000,
 		canattack = true,
 		canstop = 1,
 		category = "ALL NOTDEFENSE NOTHOVERNOTVTOL NOTMOBILE NOTSUB NOTSUBNOTSHIP NOTVTOL WEAPON SURFACE",
