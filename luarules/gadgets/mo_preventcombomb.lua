c937ffab7d25ddd1c8ea3129705bcd9c259c6d8a
sqrtone <54681493+sqrtone@users.noreply.github.com>
2020-04-25 12:06:37 +0200

    Unify comDefs, read from config file.

--- luarules/gadgets/mo_preventcombomb.lua
+++ luarules/gadgets/mo_preventcombomb.lua
@@ -89,33 +89,7 @@ local DGUN = {
   [WeaponDefNames['talon_com7_tal_disintegrator'].id] = true,
 }
 
-local COMMANDER = {
-  --Core
-  [UnitDefNames["corcom"].id] = true,
-  [UnitDefNames["corcom1"].id] = true,
-  [UnitDefNames["corcom3"].id] = true,
-  [UnitDefNames["corcom_fusion"].id] = true,
-  [UnitDefNames["corcom5"].id] = true,
-  [UnitDefNames["corcom6"].id] = true,
-  [UnitDefNames["corcom7"].id] = true,
-  --Arm
-  [UnitDefNames["armcom"].id] = true,
-  [UnitDefNames["armcom1"].id] = true,
-  [UnitDefNames["armcom4"].id] = true,
-  [UnitDefNames["armcom_fusion"].id] = true,
-  [UnitDefNames["armcom5"].id] = true,
-  [UnitDefNames["armcom6"].id] = true,
-  [UnitDefNames["armcom7"].id] = true,
-  --The lost legacy
-  [UnitDefNames["tllcom"].id] = true,
-  [UnitDefNames["tllcom1"].id] = true,
-  [UnitDefNames["tllcom3"].id] = true,
-  [UnitDefNames["tllcom_fusion"].id] = true,
-  [UnitDefNames["tllcom5"].id] = true,
-  [UnitDefNames["tllcom6"].id] = true,
-  [UnitDefNames["tllcom7"].id] = true,
-}
-
+local COMMANDER = VFS.Include("luarules/configs/comDefs.lua")
 
 local immuneDgunList = {}
 local ctrlCom = {}
