94a5b4e086e240b4922a43176c44800c43217336
skymyj <skymyj@hotmail.com>
2020-11-07 16:09:03 +0100

    Rework armsiege weapon

--- units/armsiege.lua
+++ units/armsiege.lua
@@ -86,6 +86,9 @@ return {
 			},
 		},
 		sfxtypes = {
+			explosiongenerators = {
+				[1] = "custom:goliathflare",
+			},
 			pieceexplosiongenerators = {
 				[1] = "piecetrail0",
 				[2] = "piecetrail1",
