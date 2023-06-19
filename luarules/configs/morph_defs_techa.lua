e91d49d6700c2b2d4681e68ab69876bab0d45daf
skymyj <skymyj@hotmail.com>
2023-06-19 17:58:28 +0200

    feat: remove rumad galacticus from build menu temporarly

--- luarules/configs/morph_defs_techa.lua
+++ luarules/configs/morph_defs_techa.lua
@@ -703,20 +703,20 @@ local morphDefs = {
 		tech = 3,
 		xp = 0.10,
 	},
-	rumad_mongol = {
-		{
-			into = 'rumad_gala',
-			time = 45,
-			--tech = 3,
-			xp = 0.05,
-		},
-		{
-			into = 'rumad_hellion',
-			time = 60,
-			--tech = 3,
-			xp = 0.10,
-		},
-	},
+	-- rumad_mongol = {
+	-- 	{
+	-- 		into = 'rumad_gala',
+	-- 		time = 45,
+	-- 		--tech = 3,
+	-- 		xp = 0.05,
+	-- 	},
+	-- 	{
+	-- 		into = 'rumad_hellion',
+	-- 		time = 60,
+	-- 		--tech = 3,
+	-- 		xp = 0.10,
+	-- 	},
+	-- },
 
 	-- Titan
 	cormkl = {
