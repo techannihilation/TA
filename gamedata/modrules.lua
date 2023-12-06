4e4f0092c951fae758ffdf46eb91846c3345e98a
silvapp <97389677+silvapp@users.noreply.github.com>
2023-12-06 07:45:36 +0100

    Modrules update for QTPFS v2

--- gamedata/modrules.lua
+++ gamedata/modrules.lua
@@ -63,11 +63,17 @@ local modrules = {
   },
 
   system = {
-    pathFinderSystem = 0, -- always default pathfinder
+    pathFinderSystem = 1, -- Which pathfinder does the game use? Can be 0 - The legacy default pathfinder, 1 - Quad-Tree Pathfinder System (QTPFS) or -1 - disabled.    
     pathFinderRawDistMult = 100000, -- default: 1.25.  Engine does raw move with a limited distance, this multiplier adjusts that
-    pathFinderUpdateRate = 0.0000001, -- default: 0.007.  Controls how often the pathfinder updates; larger values means more rapid updates.
-    --pathFinderUpdateRateScale = 1.0,
-    allowTake = true,       -- Enables and disables the /take UI command.
+    --pathFinderUpdateRate = 0.0000001, -- default: 0.007.  Controls how often the pathfinder updates; larger values means more rapid updates.
+    --pathFinderUpdateRateScale = 1.0,    
+    pfHcostMult = 0.2,  -- default: 0.2.  A float value between 0 and 2. Controls
+    pfRawMoveSpeedThreshold = 0,  -- default: 0.  Controls the speed modifier (which includes typemap boosts and up/down hill modifiers) under which units will never do raw move, regardless of distance etc. Defaults to 0, which means units will not try to raw-move into unpathable terrain (e.g. typemapped lava, cliffs, water). You can set it to some positive value to make them avoid pathable but very slow terrain (for example if you set it to 0.2 then they will not raw-move across terrain where they move at 20% speed or less, and will use normal pathing instead - which may still end up taking them through that path).
+    pfRepathDelayInFrames = 60, -- default: 60.  How many frames at least must pass between checks for whether a unit is making enough progress to its current waypoint or whether a new path should be requested
+    pfRepathMaxRateInFrames = 150,  -- default: 150.  Controls the minimum amount of frames that must pass before a unit is allowed to request a new path. Mostly for rate limiting and prevent excessive CPU wastage
+    pfUpdateRateScale = 1,  -- default: 1.  Multiplier for the update rate
+
+    allowTake = true, -- Enables and disables the /take UI command.
     LuaAllocLimit = 2560, -- default: 1536.  Global Lua alloc limit (in megabytes)
   },
 
