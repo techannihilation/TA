c7f4cef55babef41209c8605734169c0571f0e31
René Jochum <rene@jochum.dev>
2023-12-13 08:36:28 +0100

    feat: sun parameters.

--- luarules/gadgets/api_custom_unit_shaders.lua
+++ luarules/gadgets/api_custom_unit_shaders.lua
@@ -32,7 +32,9 @@ if (gadgetHandler:IsSyncedCode()) then
 	return
 end
 
-
+-- TODO(jochumdev): Move to better place.
+Spring.SetSunDirection(0)
+Spring.SetSunLighting({groundAmbientColor = {1, 0.1, 1}, groundDiffuseColor = {1, 0.1, 1} })
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
