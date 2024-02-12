13e0d27f516ca1f2376c685c94c3784d17a2294b
silvapp <97389677+silvapp@users.noreply.github.com>
2024-02-12 19:21:08 +0100

    Update modrules.lua

--- gamedata/modrules.lua
+++ gamedata/modrules.lua
@@ -21,7 +21,7 @@ local modrules = {
     experienceMult = 1.0, -- defaults to 1.0
     powerScale = 0.0, -- defaults to 1.0
     healthScale = 0.25, -- defaults to 0.7
-    reloadScale = 1.1, -- defaults to 0.4
+    reloadScale = 2.4, -- defaults to 0.4
   },
 
   featureLOS = {
