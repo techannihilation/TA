3bc549b3d797589bf2cc4c711550f3ba0032a42d
silvapp <97389677+silvapp@users.noreply.github.com>
2024-02-29 19:27:26 +0100

    Revert autocapture, and make it def off

--- luarules/gadgets/unit_autocapture.lua
+++ luarules/gadgets/unit_autocapture.lua
@@ -1,93 +1,101 @@
 function gadget:GetInfo()
     return {
-        name = 'Unit Auto Capture',
-        desc = 'Manage support for automatically initiate capture',
-        author = 'tizbac',
-        date = '16.12.2023',
-        license = 'GPL',
-        layer = 0,
-        enabled = true
+      name      = "Unit Auto Capture",
+      desc      = "Manage support for automatically initiate capture",
+      author    = "tizbac",
+      date      = "",
+      license   = "GPL",
+      layer     = 0,
+      enabled   = true  --  loaded by default?
     }
 end
 
 local CMD_AUTOCAPTURE_TOGGLE = 33010
+
+if (not gadgetHandler:IsSyncedCode()) then
+  return false
+end
+
 local autocapture_units = {
-    [UnitDefNames['rumad_cordyceps'].id] = true,
-    [UnitDefNames['corcommando'].id] = true,
-    [UnitDefNames['talon_shepherd'].id] = true,
-    [UnitDefNames['tllgizmo'].id] = true,
-    [UnitDefNames['armrambo'].id] = true,
-    [UnitDefNames['rumad_parasite'].id] = true,    
+    [UnitDefNames["rumad_cordyceps"].id] = true,
+    [UnitDefNames["corcommando"].id] = true,
+    [UnitDefNames["talon_shepherd"].id] = true,
+    [UnitDefNames["tllgizmo"].id] = true,
+    [UnitDefNames["armrambo"].id] = true, 
+    [UnitDefNames["rumad_parasite"].id] = true,       
 }
+
 local autocapture_enabled = {}
 
-local spGetUnitCommands = Spring.GetUnitCommands
-local spGetUnitPosition = Spring.GetUnitPosition
-local spGetUnitAllyTeam = Spring.GetUnitAllyTeam
-local spGetUnitsInCylinder = Spring.GetUnitsInCylinder
-local spGetUnitIsDead = Spring.GetUnitIsDead
-local spGetUnitDefID = Spring.GetUnitDefID
-local spGiveOrderToUnit = Spring.GiveOrderToUnit
-local spInsertUnitCmdDesc = Spring.InsertUnitCmdDesc
-local spFindUnitCmdDesc = Spring.FindUnitCmdDesc
-local spEditUnitCmdDesc = Spring.EditUnitCmdDesc
+local autoCapture = {
+    id			= CMD_AUTOCAPTURE_TOGGLE,
+    type		= CMDTYPE.ICON_MODE,
+    name		= "Autocapture",
+    tooltip     = "Capture nearby units automatically",
+    params  = { "0", "Autocapture\n     Off", "Autocapture\n    On"}
+}
+function gadget:Initialize()
+    gadgetHandler:RegisterCMDID(CMD_AUTOCAPTURE_TOGGLE)
+end
 
-if gadgetHandler:IsSyncedCode() then
-    function gadget:Initialize()
-        gadgetHandler:RegisterCMDID(CMD_AUTOCAPTURE_TOGGLE)
+
+function gadget:UnitCreated(uID, unitDefID, teamID)
+    if autocapture_units[unitDefID] ~= nil then
+        Spring.InsertUnitCmdDesc(uID, autoCapture)
+        local unitDef = UnitDefs[unitDefID]
+        autocapture_enabled[uID] = nil
     end
+end
 
-    function gadget:UnitCreated(uID, unitDefID, teamID)
-        if autocapture_units[unitDefID] then
-            spInsertUnitCmdDesc(uID, {
-                id = CMD_AUTOCAPTURE_TOGGLE,
-                type = CMDTYPE.ICON_MODE,
-                name = 'Autocapture',
-                tooltip = 'Capture nearby units automatically',
-                params = { '0', 'Autocapture\n     Off', 'Autocapture\n    On'}
-            })
-            autocapture_enabled[uID] = UnitDefs[unitDefID].buildDistance
+function gadget:UnitCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions)
+    if autocapture_units[unitDefID] ~= nil and cmdID == CMD_AUTOCAPTURE_TOGGLE then
+        local unitDef = UnitDefs[unitDefID]
+        local cmdDescID = Spring.FindUnitCmdDesc(unitID, CMD_AUTOCAPTURE_TOGGLE)
+        autoCapture.params[1] = cmdParams[1]
+        Spring.EditUnitCmdDesc(unitID, cmdDescID, {
+            params  = autoCapture.params,
+        })
+        --Spring.Echo(cmdParams[1].." "..unitDef.buildDistance)
+        if cmdParams[1] == 1 then
+            autocapture_enabled[unitID] = unitDef.buildDistance
+        else
+            autocapture_enabled[unitID] = nil
         end
     end
+end
 
-    function gadget:UnitCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions)
-        if autocapture_units[unitDefID] and cmdID == CMD_AUTOCAPTURE_TOGGLE then
-            local cmdDescID = spFindUnitCmdDesc(unitID, CMD_AUTOCAPTURE_TOGGLE)
-            spEditUnitCmdDesc(unitID, cmdDescID, {
-                params = {cmdParams[1], 'Autocapture\n     Off', 'Autocapture\n    On'}
-            })
-            autocapture_enabled[unitID] = cmdParams[1] == 1 and UnitDefs[unitDefID].buildDistance or nil
-        end
-    end
+function gadget:UnitDestroyed(unitID)
+    autocapture_enabled[unitID] = nil
+end
 
-    function gadget:UnitDestroyed(unitID)
-        autocapture_enabled[unitID] = nil
-    end
+function gadget:GameFrame(n)
+ if n % 20 == 0 then
+    for unitid, dist in pairs(autocapture_enabled) do
+        local cmds = Spring.GetUnitCommands(unitid,5)
+        if #cmds > 0 and cmds[1].id == CMD.CAPTURE then
+            --Spring.Echo("Already capturing")
 
-    function gadget:GameFrame(n)
-        if n % 20 == 0 then
-            for unitID, dist in pairs(autocapture_enabled) do
-                if #spGetUnitCommands(unitID, 5) > 0 and spGetUnitCommands(unitID, 5)[1].id == CMD.CAPTURE then
-                    -- Do nothing if already capturing
-                else
-                    local x, _, z = spGetUnitPosition(unitID)
-                    local myAlly = spGetUnitAllyTeam(unitID)
-                    local targets = spGetUnitsInCylinder(x, z, dist)
-                    for _, target in ipairs(targets) do
-                        if spGetUnitAllyTeam(target) ~= myAlly and not spGetUnitIsDead(target) then
-                            local targetDefID = spGetUnitDefID(target)
-                            if targetDefID and not UnitDefs[targetDefID].canFly then
-                                local cmds = spGetUnitCommands(unitID, 5)
-                                spGiveOrderToUnit(unitID, CMD.CAPTURE, {target}, {''})
-                                for _, cmd in ipairs(cmds) do
-                                    spGiveOrderToUnit(unitID, cmd.id, cmd.params, {'shift'})
-                                end
-                                break
-                            end
-                        end
+        else
+            local x,y,z = Spring.GetUnitPosition(unitid)
+            local myally = Spring.GetUnitAllyTeam(unitid)
+            --Spring.Echo("Checking "..unitid.." "..x.." "..z.." "..dist)
+            local targets = CallAsTeam(Spring.GetUnitTeam(unitid), Spring.GetUnitsInCylinder, x, z, dist)
+            for i,target in ipairs(targets) do
+                if Spring.GetUnitAllyTeam(target) ~= myally then
+                    --Spring.Echo(target)
+                      --//copy command queue
+                    local cmds = Spring.GetUnitCommands(unitid,5) --only copy last 4 command as first is skipped
+
+                    Spring.GiveOrderToUnit(unitid, CMD.CAPTURE, { target}, {""})
+                    for i = 1, #cmds do  -- skip the first command (CMD_MORPH)
+                        local cmd = cmds[i]
+                        Spring.GiveOrderToUnit(unitid, cmd.id, cmd.params, {"shift"})
                     end
+
+                    break -- Only queue ONE capture command
                 end
             end
         end
     end
+ end
 end
