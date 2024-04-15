ce9b1144da131d37fc1879ec68403fd687677132
silvapp <97389677+silvapp@users.noreply.github.com>
2024-04-15 20:04:10 +0200

    Upd nano boost + coms

---
+++ luarules/configs/nanoDefIDs.lua
@@ -0,0 +1,62 @@
+local nanos = {
+  --Arm
+  [UnitDefNames['armnanotc'].id] = true,
+  [UnitDefNames['armnanotc1'].id] = true,
+  [UnitDefNames['armnanotc2'].id] = true,
+  [UnitDefNames['armnanotc3'].id] = true,
+  [UnitDefNames['armnanotc4'].id] = true,
+  [UnitDefNames['armfnanotc'].id] = true,
+  [UnitDefNames['armfnanotc1'].id] = true,
+  [UnitDefNames['armfnanotc2'].id] = true,
+  [UnitDefNames['armfnanotc3'].id] = true,
+  --Core
+  [UnitDefNames['cornanotc'].id] = true,
+  [UnitDefNames['cornanotc1'].id] = true,
+  [UnitDefNames['cornanotc2'].id] = true,
+  [UnitDefNames['cornanotc3'].id] = true,
+  [UnitDefNames['cornanotc4'].id] = true,
+  [UnitDefNames['corfnanotc'].id] = true,
+  [UnitDefNames['corfnanotc1'].id] = true,
+  [UnitDefNames['corfnanotc2'].id] = true,
+  [UnitDefNames['corfnanotc3'].id] = true,
+  --Tll
+  [UnitDefNames['tllnanotc'].id] = true,
+  [UnitDefNames['tllnanotc1'].id] = true,
+  [UnitDefNames['tllnanotc2'].id] = true,
+  [UnitDefNames['tllnanotc3'].id] = true,
+  [UnitDefNames['tllnanotc4'].id] = true,
+  [UnitDefNames['tllfnanotc'].id] = true,
+  [UnitDefNames['tllfnanotc1'].id] = true,
+  [UnitDefNames['tllfnanotc2'].id] = true,
+  [UnitDefNames['tllfnanotc3'].id] = true,
+  --Talon
+  [UnitDefNames['talon_nanotc'].id] = true,
+  [UnitDefNames['talon_nanotc1'].id] = true,
+  [UnitDefNames['talon_nanotc2'].id] = true,
+  [UnitDefNames['talon_nanotc3'].id] = true,
+  [UnitDefNames['talon_nanotc4'].id] = true,
+  [UnitDefNames['talon_fnanotc'].id] = true,
+  [UnitDefNames['talon_fnanotc1'].id] = true,
+  [UnitDefNames['talon_fnanotc2'].id] = true,
+  [UnitDefNames['talon_fnanotc3'].id] = true,
+
+  --Gok
+  [UnitDefNames['gok_nanotc'].id] = true,
+  [UnitDefNames['gok_nanotc1'].id] = true,
+  [UnitDefNames['gok_nanotc2'].id] = true,
+  [UnitDefNames['gok_nanotc3'].id] = true,
+  [UnitDefNames['gok_nanotc4'].id] = true,
+  [UnitDefNames['gok_fnanotc'].id] = true,
+  [UnitDefNames['gok_fnanotc1'].id] = true,
+  [UnitDefNames['gok_fnanotc2'].id] = true,
+  [UnitDefNames['gok_fnanotc3'].id] = true,
+
+  --Rumad
+  [UnitDefNames['rumad_nanotc'].id] = true,
+  [UnitDefNames['rumad_nanotc1'].id] = true,
+  [UnitDefNames['rumad_nanotc2'].id] = true,
+  [UnitDefNames['rumad_nanotc3'].id] = true,
+  [UnitDefNames['rumad_nanotc4'].id] = true,
+}
+
+return nanos
--- luarules/gadgets/unit_nanoboost.lua
+++ luarules/gadgets/unit_nanoboost.lua
@@ -6,7 +6,7 @@ function gadget:GetInfo()
   return {
     name      = 'Nano Boost',
     desc      = 'Turbo mode for nanos',
-    author    = 'Nixtux', -- based on production rate gadget by Deadnight Warrior
+    author    = 'Nixtux, Silver', -- based on production rate gadget by Deadnight Warrior
     date      = 'Oct 20, 2007',
     license   = 'GNU GPL, v2 or later',
     layer     = 0,
@@ -36,76 +36,12 @@ local SpGetUnitHealth = Spring.GetUnitHealth
 local SpAddUnitDamage = Spring.AddUnitDamage
 local spSetUnitRulesParam  = Spring.SetUnitRulesParam
 local gaiaTeamID = Spring.GetGaiaTeamID()
-
-local mrandom = math.random
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
+local comDefs = VFS.Include('luarules/configs/comDefIDs.lua')
+local nanos = VFS.Include('luarules/configs/comDefIDs.lua')
 
 CMD_NANOBOOST = 33456
 local boostednanos = {} -- list of nanos in boostmode
-
 local buildspeedlist = {}
-
-local nanos = {
-  --Arm
-  [UnitDefNames['armnanotc'].id] = true,
-  [UnitDefNames['armnanotc1'].id] = true,
-  [UnitDefNames['armnanotc2'].id] = true,
-  [UnitDefNames['armnanotc3'].id] = true,
-  [UnitDefNames['armnanotc4'].id] = true,
-  [UnitDefNames['armfnanotc'].id] = true,
-  [UnitDefNames['armfnanotc1'].id] = true,
-  [UnitDefNames['armfnanotc2'].id] = true,
-  [UnitDefNames['armfnanotc3'].id] = true,
-  --Core
-  [UnitDefNames['cornanotc'].id] = true,
-  [UnitDefNames['cornanotc1'].id] = true,
-  [UnitDefNames['cornanotc2'].id] = true,
-  [UnitDefNames['cornanotc3'].id] = true,
-  [UnitDefNames['cornanotc4'].id] = true,
-  [UnitDefNames['corfnanotc'].id] = true,
-  [UnitDefNames['corfnanotc1'].id] = true,
-  [UnitDefNames['corfnanotc2'].id] = true,
-  [UnitDefNames['corfnanotc3'].id] = true,
-  --Tll
-  [UnitDefNames['tllnanotc'].id] = true,
-  [UnitDefNames['tllnanotc1'].id] = true,
-  [UnitDefNames['tllnanotc2'].id] = true,
-  [UnitDefNames['tllnanotc3'].id] = true,
-  [UnitDefNames['tllnanotc4'].id] = true,
-  [UnitDefNames['tllfnanotc'].id] = true,
-  [UnitDefNames['tllfnanotc1'].id] = true,
-  [UnitDefNames['tllfnanotc2'].id] = true,
-  [UnitDefNames['tllfnanotc3'].id] = true,
-  --Talon
-  [UnitDefNames['talon_nanotc'].id] = true,
-  [UnitDefNames['talon_nanotc1'].id] = true,
-  [UnitDefNames['talon_nanotc2'].id] = true,
-  [UnitDefNames['talon_nanotc3'].id] = true,
-  [UnitDefNames['talon_nanotc4'].id] = true,
-  [UnitDefNames['talon_fnanotc'].id] = true,
-  [UnitDefNames['talon_fnanotc1'].id] = true,
-  [UnitDefNames['talon_fnanotc2'].id] = true,
-  [UnitDefNames['talon_fnanotc3'].id] = true,
-
-  --Gok
-  [UnitDefNames['gok_nanotc'].id] = true,
-  [UnitDefNames['gok_nanotc1'].id] = true,
-  [UnitDefNames['gok_nanotc2'].id] = true,
-  [UnitDefNames['gok_nanotc3'].id] = true,
-  [UnitDefNames['gok_nanotc4'].id] = true,
-  [UnitDefNames['gok_fnanotc'].id] = true,
-  [UnitDefNames['gok_fnanotc1'].id] = true,
-  [UnitDefNames['gok_fnanotc2'].id] = true,
-  [UnitDefNames['gok_fnanotc3'].id] = true,
-
-  --Rumad
-  [UnitDefNames['rumad_nanotc'].id] = true,
-  [UnitDefNames['rumad_nanotc1'].id] = true,
-  [UnitDefNames['rumad_nanotc2'].id] = true,
-  [UnitDefNames['rumad_nanotc3'].id] = true,
-  [UnitDefNames['rumad_nanotc4'].id] = true,
-}
 
 local buildspeedCmdDesc = {
   id      = CMD_NANOBOOST,
@@ -116,8 +52,6 @@ local buildspeedCmdDesc = {
   tooltip = 'Divert All power to Construction',
   params  = { '0', 'Boost Off', 'Boost On'}
 }
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
 
 local function AddBuildspeedCmdDesc(unitID)
   if (FindUnitCmdDesc(unitID, CMD_NANOBOOST)) then
@@ -132,11 +66,6 @@ local function AddBuildspeedCmdDesc(unitID)
     FindUnitCmdDesc(unitID, CMD.FIRE_STATE) or
     123456 -- back of the pack
 
-    --Remove unused button
-    removeButton(unitID, CMD.ATTACK)
-    removeButton(unitID, CMD.MOVE)
-    --removeButton(unitID, CMD.FIGHT)
-    removeButton(unitID, CMD.FIRE_STATE)
     buildspeedCmdDesc.params[1] = '1'
     spInsertUnitCmdDesc(unitID, insertID + 1, buildspeedCmdDesc)
 end
@@ -163,13 +92,11 @@ end
 
 local function BuildspeedCommand(unitID, unitDefID, cmdParams, teamID)
     if cmdParams[1] == 1 then
-        --Spring.Echo('boosted at ' .. buildspeedlist[unitID].speed *1.8)
         Spring.SetUnitBuildSpeed(unitID, buildspeedlist[unitID].speed * 5.0, buildspeedlist[unitID].repair * 2.5, buildspeedlist[unitID].reclaim * 5.0)
         spSetUnitRulesParam(unitID, 'nanoPower', buildspeedlist[unitID].speed * 5.0)
         spSetUnitRulesParam(unitID, 'nanoBoosted', 1)
         boostednanos[unitID] = true
     else
-        --Spring.Echo('not boosted at ' .. buildspeedlist[unitID].speed)
         Spring.SetUnitBuildSpeed(unitID, buildspeedlist[unitID].speed, buildspeedlist[unitID].repair, buildspeedlist[unitID].reclaim)
         spSetUnitRulesParam(unitID, 'nanoPower', buildspeedlist[unitID].speed)
         spSetUnitRulesParam(unitID, 'nanoBoosted', 0)
@@ -187,9 +114,6 @@ function removeButton(unitID, ID)
         spRemoveUnitCmdDesc(unitID, cmdID)
     end
 end
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
 
 local boostednanosQueue = {}
 local currentDamageCoroutine = nil
@@ -231,8 +155,8 @@ function processDamage()
         local unitID = table.remove(boostednanosQueue, 1)
         local curhp, hp = SpGetUnitHealth(unitID)
 
-        local stdDev = 0.15
-        local constDmg = 0.00025
+        local stdDev = 0.2
+        local constDmg = 0.0004
 
         local damage = math.abs(curhp * (stdDev * getGaussian())) + (hp * constDmg)
 
@@ -278,7 +202,7 @@ end
 
 function gadget:UnitCreated(unitID, unitDefID, teamID, builderID)
     local ud = uDefs[unitDefID]
-    if nanos[unitDefID] then
+    if nanos[unitDefID] or comDefs[unitDefID] then
         local stMode = 0
         buildspeedlist[unitID]={speed=ud.buildSpeed, repair=ud.repairSpeed, reclaim=ud.reclaimSpeed, mode=stMode}
         AddBuildspeedCmdDesc(unitID)
@@ -310,11 +234,7 @@ function gadget:Shutdown()
     end
 end
 
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
---synced
-else
+else -- unsynced part here
 
 function gadget:Initialize()
   gadgetHandler:AddSyncAction('boostsound', BoostSound)
