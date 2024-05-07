2193b3bcddeb68b5e31e3bff0815ed8f4afb1777
silvapp <97389677+silvapp@users.noreply.github.com>
2024-05-07 18:50:39 +0200

    fix

--- luarules/gadgets/unit_spam_limiter.lua
+++ luarules/gadgets/unit_spam_limiter.lua
@@ -1,7 +1,7 @@
 function gadget:GetInfo()
     return {
         name = "UnitBuildTimeAdjuster",
-        desc = "Monitors unit production and adjusts build time based on quantity created and team resources",
+        desc = "Monitors unit mass production and adjusts build time",
         author = "[MOL]Silver",
         date = "2023-11-05",
         license = "GNU GPL, v2 or later",
@@ -11,38 +11,32 @@ function gadget:GetInfo()
 end
 
 if gadgetHandler:IsSyncedCode() then
-    local spGetUnitDefID = Spring.GetUnitDefID
     local spSetUnitCosts = Spring.SetUnitCosts
     local spGetTeamResources = Spring.GetTeamResources
-    local math_smoothstep = math.smoothstep
     local playerUnitCount = {}
     local buildTimeCache = {}
-    local isFactoryCache = {}
     local UnitDefs = UnitDefs
-    local unitThreshhold = 10
 
     function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
+
         if builderID then
-            local builderDefID = spGetUnitDefID(builderID)
-            isFactoryCache[builderDefID] = isFactoryCache[builderDefID] or UnitDefs[builderDefID].isFactory
-            if isFactoryCache[builderDefID] then
-                local playerData = playerUnitCount[unitTeam] or {}
-                playerUnitCount[unitTeam] = playerData
-                playerData[unitDefID] = (playerData[unitDefID] or 0) + 1
-                local count = playerData[unitDefID]
-                if count > unitThreshhold then
-                    local _, _, _, energyIncome = spGetTeamResources(unitTeam, "energy")
-                    local baseEnergy = 15000
-                    local maxEnergy = 3000000
-                    local penaltyMultiplayer = 1.5
-                    if energyIncome < baseEnergy then return end
-                    local penaltyFactor = math_smoothstep(baseEnergy, maxEnergy, energyIncome) * penaltyMultiplayer
-                    buildTimeCache[unitDefID] = buildTimeCache[unitDefID] or UnitDefs[unitDefID].buildTime
-                    local newBuildTime = buildTimeCache[unitDefID] * (1 + ((count - unitThreshhold) * penaltyFactor) )
-                    spSetUnitCosts(unitID, {buildTime = newBuildTime})
-                    --Spring.Echo(newBuildTime,penaltyFactor,count)
-                end
-            end
+            local _, _, _, energyIncome = spGetTeamResources(unitTeam, "energy")
+            local baseEnergy = 3000
+            local maxEnergy = 3000000
+            local multiplier  = 0.5
+            if energyIncome < baseEnergy then return end
+
+            local playerData = playerUnitCount[unitTeam] or {}
+            playerUnitCount[unitTeam] = playerData
+            playerData[unitDefID] = (playerData[unitDefID] or 0) + 1
+
+            local count = playerData[unitDefID]
+            local penaltyFactor = (energyIncome - baseEnergy) / (maxEnergy - baseEnergy) * multiplier
+            buildTimeCache[unitDefID] = buildTimeCache[unitDefID] or UnitDefs[unitDefID].buildTime
+
+            local newBuildTime = buildTimeCache[unitDefID] * (1 + (count * penaltyFactor))
+            spSetUnitCosts(unitID, {buildTime = newBuildTime})
+
         end
     end
 
@@ -56,9 +50,9 @@ if gadgetHandler:IsSyncedCode() then
         if playerUnitCount[oldTeam] and playerUnitCount[oldTeam][unitDefID] then
             playerUnitCount[oldTeam][unitDefID] = playerUnitCount[oldTeam][unitDefID] - 1
         end
+
         if playerUnitCount[newTeam] and playerUnitCount[newTeam][unitDefID] then
             playerUnitCount[newTeam][unitDefID] = playerUnitCount[newTeam][unitDefID] + 1
         end
     end
-
 end
