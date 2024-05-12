0c8e7e6b2f1f12c8528ea73414c2107153585934
silvapp <97389677+silvapp@users.noreply.github.com>
2024-05-12 12:53:15 +0200

    Update unit_spam_limiter.lua

--- luarules/gadgets/unit_spam_limiter.lua
+++ luarules/gadgets/unit_spam_limiter.lua
@@ -1,48 +1,47 @@
 function gadget:GetInfo()
     return {
-        name = "UnitBuildTimeAdjuster",
-        desc = "Monitors unit mass production and adjusts build time",
-        author = "[MOL]Silver",
-        date = "2023-11-05",
-        license = "GNU GPL, v2 or later",
+        name = 'UnitBuildTimeAdjuster',
+        desc = 'Monitors unit mass production and adjusts build time',
+        author = '[MOL]Silver',
+        date = '2024-12-05',
+        license = 'GNU GPL, v2 or later',
         layer = 0,
         enabled = true,
     }
 end
 
--- local spamLimit = Spring.GetModOptions().exp_spamlimit or false
---Spring.Echo("SPAMLIMIT STATUS: ",spamLimit)
--- if spamLimit == false then
---     return
--- end
+local spamLimit = Spring.GetModOptions().exp_spamlimit or false
+
+if spamLimit == false then
+    return
+end
 
 if gadgetHandler:IsSyncedCode() then
+    local nanos = VFS.Include('luarules/configs/nanoDefIDs.lua')
     local spSetUnitCosts = Spring.SetUnitCosts
     local spGetTeamResources = Spring.GetTeamResources
+    local math_min = math.min
     local playerUnitCount = {}
     local buildTimeCache = {}
     local UnitDefs = UnitDefs
 
     function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
-
-        if builderID then
-            local _, _, _, energyIncome = spGetTeamResources(unitTeam, "energy")
-            local baseEnergy = 5000
-            local maxEnergy = 10000000
-            local multiplier  = 0.333
+        if builderID and not nanos[unitDefID] then
+            local _, _, _, energyIncome = spGetTeamResources(unitTeam, 'energy')
+            local baseEnergy = 2000
+            local maxEnergy = 20000000
+            local maxpenatlyFactor = 5
             if energyIncome < baseEnergy then return end
-
             local playerData = playerUnitCount[unitTeam] or {}
             playerUnitCount[unitTeam] = playerData
             playerData[unitDefID] = (playerData[unitDefID] or 0) + 1
-
             local count = playerData[unitDefID]
-            local penaltyFactor = (energyIncome - baseEnergy) / (maxEnergy - baseEnergy) * multiplier
+            local energyFactor = (energyIncome - baseEnergy) / (maxEnergy - baseEnergy)
+            local penatlyFactor = math_min(1 + (count * energyFactor), maxpenatlyFactor)
             buildTimeCache[unitDefID] = buildTimeCache[unitDefID] or UnitDefs[unitDefID].buildTime
-
-            local newBuildTime = buildTimeCache[unitDefID] * (1 + (count * penaltyFactor))
+            local newBuildTime = buildTimeCache[unitDefID] * penatlyFactor
+            --Spring.Echo('count = ' .. count .. '  ', unitDefID, penatlyFactor, newBuildTime)
             spSetUnitCosts(unitID, {buildTime = newBuildTime})
-
         end
     end
 
