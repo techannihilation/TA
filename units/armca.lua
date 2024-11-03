c060c7615606349fff0146775b931b7d2e08358a
silvapp <97389677+silvapp@users.noreply.github.com>
2024-11-03 15:46:06 +0100

    Repleace grape with beamer in aircon

--- units/armca.lua
+++ units/armca.lua
@@ -81,7 +81,7 @@ return {
 			[23] = "armdrag",
 			[24] = "armclaw",
 			[25] = "armllt",
-			[26] = "armgrape",
+			[26] = "armbeamer",
 			[27] = "armhlt",
 			[28] = "armguard",
 			[29] = "armbrtha2",
