95ff1bee33477ca3d4c38fb23e91fc650d029ae2
silvapp <97389677+silvapp@users.noreply.github.com>
2025-01-17 18:20:35 +0100

    Update customcmds.lua

--- luarules/configs/customcmds.lua
+++ luarules/configs/customcmds.lua
@@ -1,8 +1,17 @@
 local commands = {
-    UPG_SPEED       = 1000,
-    UPG_ARMOR       = 1001,
-    UPG_CLOAK       = 1002,
-    UPG_BUILDPWR    = 1003,
+    ARMORED                     = 34590,
+    COBBUTTON                   = 34500,
+    COBBUTTON_MAX               = 34500 + 50,
+    LAND_AT_AIRBASE             = 35430,
+    LAND_AT_SPECIFIC_AIRBASE    = 35431,
+    WANTED_SPEED                = 38825,
+    LAND                        = 34569,
+    AIR                         = 34570,
+    UPG_SPEED                   = 1000,
+    UPG_ARMOR                   = 1001,
+    UPG_CLOAK                   = 1002,
+    UPG_BUILDPWR                = 1003,
+    UPG_STEALTH                 = 1004,
 }
 
 return commands
