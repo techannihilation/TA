128446f8499404fa7c9e2f3e2cb1bc42bc573f5a
silvapp <97389677+silvapp@users.noreply.github.com>
2025-01-18 09:47:23 +0100

    Update unit_upgrades.lua

--- luarules/gadgets/unit_upgrades.lua
+++ luarules/gadgets/unit_upgrades.lua
@@ -65,9 +65,9 @@ local CMD_UPG_STEALTH       = Spring.Utilities.CMD.UPG_STEALTH
 local SPEED_BOOST_FACTOR    = 1.80
 local SPEED_COST_MULT       = 1.50
 
-local ARMOR_BOOST_FACTOR    = 81.00
+local ARMOR_BOOST_FACTOR    = 91.00
 local ARMOR_COST_MULT       = 100.00
-local ARMOR_SPEED_PENALTY   = 0.50
+local ARMOR_SPEED_PENALTY   = 0.60
 
 local CLOAK_COST_MULT       = 1.00
 local DECLAK_DISTANCE_MULT  = 0.05
