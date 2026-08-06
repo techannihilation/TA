84360453de5be3b83b4fbfd1f3db27f5eba52f52
skymyj <skymyj@hotmail.com>
2026-08-06 17:01:05 +0200

    Add Cegtag for tll hover AA

--- units/tllhovermissile.lua
+++ units/tllhovermissile.lua
@@ -121,6 +121,7 @@ return {
 				burst = 3,
 				burstrate = 0.2,
 				canattackground = false,
+				cegtag = "Tll_Def_AA_Rocket",
 				craterareaofeffect = 0,
 				craterboost = 0,
 				cratermult = 0,
